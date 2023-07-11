92e87d1629cd009980c365dda540401987064614
somsom13 <somin1113@naver.com>
2023-07-11 17:49:06 +0900

    config: 백엔드 프로젝트 설정

---
+++ backend/src/test/java/shook/shook/ShookApplicationTests.java
@@ -0,0 +1,13 @@
+package shook.shook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
