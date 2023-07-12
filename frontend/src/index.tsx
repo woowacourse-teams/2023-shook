392b3f78e1c306d296ac20368416681aef2b96dc
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-12 21:02:47 +0900

    refactor: import 방식 변경

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
