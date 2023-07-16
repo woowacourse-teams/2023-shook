be577e1c0cf38566da13751b8d8d9863c844eb6b
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-16 18:31:39 +0900

    feat: router 경로 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
