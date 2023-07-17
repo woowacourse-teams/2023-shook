f4587ac42b07cb0e0500cf1763811fd6ca419a87
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-17 17:59:36 +0900

    feat: 토스트 컴포넌트 구현

---
+++ frontend/src/components/@common/Toast/Toast.style.tsx
@@ -0,0 +1,22 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  width: 300px;
+  height: 50px;
+  padding: 14px;
+  border-radius: 60px;
+  background: #323232;
+`;
+
+export const StatusIcon = styled.img`
+  width: 22px;
+  height: 22px;
+`;
+
+export const Message = styled.span`
+  color: #ffffff;
+  font-size: 14px;
+`;
