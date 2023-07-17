403e00a7105342a05c6a34de7044e1d7046db689
splitube <voicesplit@daum.net>
2023-07-17 17:46:15 +0900

    refactor: 정확한 명시를 위해 Repository 어노테이션 추가

--- backend/src/main/java/shook/shook/part/domain/repository/PartRepository.java
+++ backend/src/main/java/shook/shook/part/domain/repository/PartRepository.java
@@ -2,9 +2,11 @@ package shook.shook.part.domain.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import shook.shook.part.domain.Part;
 import shook.shook.song.domain.Song;
 
+@Repository
 public interface PartRepository extends JpaRepository<Part, Long> {
 
     List<Part> findAllBySong(final Song song);
--- backend/src/main/java/shook/shook/song/domain/repository/SongRepository.java
+++ backend/src/main/java/shook/shook/song/domain/repository/SongRepository.java
@@ -2,9 +2,11 @@ package shook.shook.song.domain.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import shook.shook.song.domain.Song;
 import shook.shook.song.domain.SongTitle;
 
+@Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
 
     Optional<Song> findByTitle(final SongTitle title);
