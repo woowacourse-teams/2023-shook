f0fe16e249a8738f57b0170e71f84a149014cd1b
ukkodeveloper <ukkodeveloper@gmail.com>
2023-07-18 22:27:48 +0900

    test: 테스트 목록 그룹화

--- frontend/src/utils/validateTime.test.ts
+++ frontend/src/utils/validateTime.test.ts
@@ -1,43 +1,36 @@
 import { isValidMinSec } from './validateTime';
 
 describe('isValidMinSec: 초/분 양식이 맞는지 확인하는 함수', () => {
-  test('한글은 false를 반환한다.', () => {
+  test('순자외 문자열은 false를 반환한다.', () => {
     expect(isValidMinSec('한글')).toBe(false);
+    expect(isValidMinSec('English')).toBe(false);
   });
 
-  test('0x12312은 false를 반환한다.', () => {
+  test('십진법 이외의 진법 문자열은 false를 반환한다.', () => {
     expect(isValidMinSec('0x12312')).toBe(false);
+    expect(isValidMinSec('0b12312')).toBe(false);
+    expect(isValidMinSec('0o12312')).toBe(false);
   });
 
-  test('-1은 false를 반환한다.', () => {
+  test('0미만 혹은 60 이상 자연수는 false를 반환한다.', () => {
     expect(isValidMinSec('-1')).toBe(false);
-  });
-
-  test('61은 false를 반환한다.', () => {
-    expect(isValidMinSec('61')).toBe(false);
-  });
-
-  test('0.4은 false를 반환한다.', () => {
-    expect(isValidMinSec('0.4')).toBe(false);
-  });
-
-  test('32.4은 false를 반환한다.', () => {
-    expect(isValidMinSec('32.4')).toBe(false);
-  });
-
-  test('0은 true를 반환한다.', () => {
     expect(isValidMinSec('0')).toBe(true);
+    expect(isValidMinSec('00')).toBe(true);
+    expect(isValidMinSec('60')).toBe(false);
+    expect(isValidMinSec('1000000')).toBe(false);
   });
 
-  test('00은 true를 반환한다.', () => {
-    expect(isValidMinSec('00')).toBe(true);
+  test('0~59 사이의 자연수은 true를 반환한다.', () => {
+    expect(isValidMinSec('0')).toBe(true);
+    expect(isValidMinSec('30')).toBe(true);
+  });
+
+  test('소수는 false를 반환한다.', () => {
+    expect(isValidMinSec('32.4')).toBe(false);
+    expect(isValidMinSec('0.4')).toBe(false);
   });
 
   test('빈문자열은 true를 반환한다.', () => {
     expect(isValidMinSec('')).toBe(true);
-  });
-
-  test('30은 true를 반환한다.', () => {
-    expect(isValidMinSec('30')).toBe(true);
   });
 });
