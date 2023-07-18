ca291fbbaa0c768cc191eb44246d9a787d4a21ac
스플릿 <voicesplit@daum.net>
2023-07-18 17:24:25 +0900

    config: DB 스키마 작성 및 개발 환경 더미 데이터 스크립트 작성 (#78)

---
+++ backend/src/main/resources/schema.sql
@@ -0,0 +1,26 @@
+create table if not exists part
+(
+    id           bigint auto_increment,
+    start_second integer      not null,
+    length       varchar(255) not null check (length in ('SHORT', 'STANDARD', 'LONG')),
+    song_id      bigint,
+    created_at   timestamp(6) not null,
+    primary key (id)
+);
+create table if not exists song
+(
+    id         bigint auto_increment,
+    title      varchar(100) not null,
+    singer     varchar(50)  not null,
+    length     integer      not null,
+    video_url  text         not null,
+    created_at timestamp(6) not null,
+    primary key (id)
+);
+create table if not exists vote
+(
+    id         bigint auto_increment,
+    part_id    bigint,
+    created_at timestamp(6) not null,
+    primary key (id)
+)
