cc4447883f461972ef98c6ab428668b496e0f2b8
ukkodeveloper <ukkodeveloper@gmail.com>
2023-07-19 00:31:45 +0900

    style: return 추가

--- frontend/src/utils/convertTime.ts
+++ frontend/src/utils/convertTime.ts
@@ -1,5 +1,6 @@
-export const minSecToSeconds = ([minute, second]: [minute: number, second: number]) =>
-  minute * 60 + second;
+export const minSecToSeconds = ([minute, second]: [minute: number, second: number]) => {
+  return minute * 60 + second;
+};
 
 export const secondsToMinSec = (seconds: number): [minute: number, second: number] => {
   return [Math.floor(seconds / 60), seconds % 60];
