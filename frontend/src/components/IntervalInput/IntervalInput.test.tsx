673ac72c1b8f6b1ea9bda027d28fe9cf76d031f2
ukkodeveloper <ukkodeveloper@gmail.com>
2023-07-19 00:31:11 +0900

    test: test 오류 수정 및 유의미하게 변경

--- frontend/src/components/IntervalInput/IntervalInput.test.tsx
+++ frontend/src/components/IntervalInput/IntervalInput.test.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import IntervalInput from './IntervalInput';
 
 describe('<IntervalInput /> 컴포넌트 테스트', () => {
@@ -6,9 +7,9 @@ describe('<IntervalInput /> 컴포넌트 테스트', () => {
 
   test('4개의 input이 모두 그려진다.', () => {
     render(<IntervalInput songEnd={songEnd} />);
-    const startMin = screen.getAllByRole('textbox');
+    const inputs = screen.getAllByRole('textbox');
 
-    expect(startMin).toHaveLength(4);
+    expect(inputs).toHaveLength(4);
   });
 
   test('start 2분 10초면, end는 2분 20초다. ', () => {
@@ -22,14 +23,27 @@ describe('<IntervalInput /> 컴포넌트 테스트', () => {
     expect(endSec.value).toBe('20');
   });
 
-  test('start(4:10)가 노래(3:30)보다 길면 에러메세지를 띄운다.', () => {
+  test('start 1분 55초면, end는 2분 5초다. ', () => {
+    render(<IntervalInput songEnd={songEnd} />);
+    const [startMin, startSec, endMin, endSec] = screen.getAllByRole<HTMLInputElement>('textbox');
+
+    fireEvent.change(startMin, { target: { value: '1' } });
+    fireEvent.change(startSec, { target: { value: '55' } });
+
+    expect(endMin.value).toBe('2');
+    expect(endSec.value).toBe('5');
+  });
+
+  test('start(3:25)가 노래길이에서 구간길이를 뺀 것(3:20)보다 길면 에러메세지를 띄운다.', async () => {
     render(<IntervalInput songEnd={songEnd} />);
     const [startMin, startSec] = screen.getAllByRole<HTMLInputElement>('textbox').slice(0, 2);
 
-    fireEvent.change(startMin, { target: { value: '4' } });
-    fireEvent.change(startSec, { target: { value: '10' } });
+    await userEvent.type(startMin, '3');
+    await userEvent.type(startSec, '25');
     fireEvent.blur(startSec);
 
-    expect(screen.findByText(/이하로 구간 시작점을 잡아주세요/)).toBeInTheDocument;
+    const errorMessage = screen.getByRole('alert');
+
+    expect(errorMessage.textContent).toBeTruthy();
   });
 });
