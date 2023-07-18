232e1e819126d755dc5afeae87a5639d01cb770f
ukkodeveloper <ukkodeveloper@gmail.com>
2023-07-18 14:11:15 +0900

    fix: storybook 제목 변경

--- frontend/src/components/IntervalInput/IntervalInput.stories.tsx
+++ frontend/src/components/IntervalInput/IntervalInput.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
   component: IntervalInput,
-  title: 'Header',
+  title: 'IntervalInput',
 } satisfies Meta<typeof IntervalInput>;
 
 export default meta;
