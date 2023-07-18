aca1b2f81a34b5ef8cf42eaed9ce54a6049c99c5
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-18 20:25:43 +0900

    design: reset css 추가

--- frontend/src/styles/GlobalStyles.ts
+++ frontend/src/styles/GlobalStyles.ts
@@ -1,25 +1,45 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  * {  
-    box-sizing: border-box;
-    padding: 0;
+  * {
     margin: 0;
+    padding: 0;
+    font: inherit;
+    color: inherit;
   }
-
-  li, ul {
-    list-style: none;
+  *,
+  :after,
+  :before {
+    box-sizing: border-box;
+    flex-shrink: 0;
   }
-
+  :root {
+    -webkit-tap-highlight-color: transparent;
+    -webkit-text-size-adjust: 100%;
+    text-size-adjust: 100%;
+    cursor: default;
+    line-height: 1.5;
+    overflow-wrap: break-word;
+    -moz-tab-size: 4;
+    tab-size: 4;
+  }
+  img,  
+  video, 
+  svg {
+    display: block;
+    max-width: 100%;
+  }
+  button {
+    background: none;
+    border: 0;
+    cursor: pointer;
+  }
   a {
     text-decoration: none;
   }
-
-  input{
-    border: none;
-    box-shadow: none;
-    -webkit-box-shadow: none;
-    background-color: transparent;
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
   }
 `;
 
