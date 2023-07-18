233648267507f34d5b46565c392023ef8e00d320
ukkodeveloper <ukkodeveloper@gmail.com>
2023-07-18 14:45:18 +0900

    refactor: min/ sec 데이터를 튜플 배열로 인자 받도록 수정

--- frontend/src/utils/convertTime.test.ts
+++ frontend/src/utils/convertTime.test.ts
@@ -2,11 +2,11 @@ import { minSecToSeconds, secondsToMinSec } from './convertTime';
 
 describe('minSecToSeconds: 분/초 정보를 바탕으로 초 단위로 변환', () => {
   test('2분 30초는 150초이다.', () => {
-    expect(minSecToSeconds(2, 30)).toBe(150);
+    expect(minSecToSeconds([2, 30])).toBe(150);
   });
 
   test('0분 45초는 45초이다.', () => {
-    expect(minSecToSeconds(0, 45)).toBe(45);
+    expect(minSecToSeconds([0, 45])).toBe(45);
   });
 });
 
--- frontend/src/utils/convertTime.ts
+++ frontend/src/utils/convertTime.ts
@@ -1,9 +1,10 @@
-export const minSecToSeconds = (minute: number, second: number) => minute * 60 + second;
+export const minSecToSeconds = ([minute, second]: [minute: number, second: number]) =>
+  minute * 60 + second;
 
 export const secondsToMinSec = (seconds: number): [minute: number, second: number] => {
   return [Math.floor(seconds / 60), seconds % 60];
 };
 
 export const complyToMinSec = (minute: number, second: number, fn: (origin: number) => number) => {
-  return secondsToMinSec(fn(minSecToSeconds(minute, second)));
+  return secondsToMinSec(fn(minSecToSeconds([minute, second])));
 };
