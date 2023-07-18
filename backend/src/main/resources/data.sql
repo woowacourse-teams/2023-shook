52e4cfd6c21eb66f675129f434cd5b013055d173
splitube <voicesplit@daum.net>
2023-07-18 17:20:30 +0900

    config: DB 스키마 작성 및 개발 환경 더미 데이터 스크립트 작성

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,84 @@
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Super Shy', 'NewJeans', 155, 'https://www.youtube.com/embed/NgEaOJ7lRWY', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Seven (feat. Latto) - Clean Ver.', '정국', 186, 'https://www.youtube.com/embed/UUSbUBYqU_8',
+        now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('퀸카 (Queencard)', '(여자)아이들', 162, 'https://www.youtube.com/embed/VOcb6ZHxSjc', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('헤어지자 말해요', '박재정', 244, 'https://www.youtube.com/embed/SrQzxD8UFdM', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('I AM', 'IVE (아이브)', 208, 'https://www.youtube.com/embed/cU0JrSAyy7o', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('이브, 프시케 그리고 푸른 수염의 아내', 'LE SSERAFIM (르세라핌)', 186,
+        'https://www.youtube.com/embed/Ii8L0qEvfC8', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Spicy', 'aespa', 198, 'https://www.youtube.com/embed/1kfmWl3o8TE', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Steal The Show (From “엘리멘탈”)', 'Lauv', 194, 'https://www.youtube.com/watch?v=kUMds6XKtfY',
+        now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('사랑은 늘 도망가', '임영웅', 273, 'https://www.youtube.com/embed/pBEAzM2TRmE', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('ISTJ', 'NCT DREAM', 186, 'https://www.youtube.com/embed/es60T3k-tyM', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('모래 알갱이', '임영웅', 221, 'https://www.youtube.com/embed/3_wOZrzmQ1o', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('UNFORGIVEN (feat. Nile Rodgers)', 'LE SSERAFIM (르세라핌)', 181,
+        'https://www.youtube.com/watch?v=fzSDGXyGTjg', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Kitsch', 'IVE (아이브)', 195, 'https://www.youtube.com/watch?v=r572qh2__-U', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('우리들의 블루스', '임영웅', 207, 'https://www.youtube.com/embed/epz-aL5RaLQ', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Candy', 'NCT DREAM', 220, 'https://www.youtube.com/embed/QuaVFoBLQeg', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Hype boy', 'NewJeans', 180, 'https://www.youtube.com/embed/T--6HBX2K4g', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('다시 만날 수 있을까', '임영웅', 275, 'https://www.youtube.com/embed/VPDRLgfqfSs', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Broken Melodies', 'NCT DREAM', 227, 'https://www.youtube.com/embed/EPsh2192sTU', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Still With You', '정국', 239, 'https://www.youtube.com/embed/BksBNbTIoPE', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('무지개', '임영웅', 198, 'https://www.youtube.com/embed/o8e0Qd2H1qc', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Ditto', 'NewJeans', 187, 'https://www.youtube.com/embed/haCpjUXIhrI', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('London Boy', '임영웅', 289, 'https://www.youtube.com/embed/ZRDuScdwEbE', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('이제 나만 믿어요', '임영웅', 274, 'https://www.youtube.com/embed/y1KXYmMuZZA', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('아버지', '임영웅', 240, 'https://www.youtube.com/embed/dbaiMJOnaB4', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Polaroid', '임영웅', 209, 'https://www.youtube.com/embed/PVDxs6GUXSI', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Dynamite', '방탄소년단', 198, 'https://www.youtube.com/embed/KhZ5DCd7m6s', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('손오공', '세븐틴 (SEVENTEEN)', 200, 'https://www.youtube.com/embed/tFPbzfU5XL4', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('인생찬가', '임영웅', 235, 'https://www.youtube.com/embed/cXHduPVrcDQ', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('A bientot', '임영웅', 258, 'https://www.youtube.com/embed/sZDDLUB8wQE', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('손이 참 곱던 그대', '임영웅', 197, 'https://www.youtube.com/embed/OpZIaI-J0uk', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('사랑해 진짜', '임영웅', 241, 'https://www.youtube.com/embed/qkledxNCNfY', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('꽃', '지수', 174, 'https://www.youtube.com/embed/6zM48_rBFbY', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('연애편지', '임영웅', 217, 'https://www.youtube.com/embed/gSQFZvUuQ3s', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('New jeans', 'New jeans', 109, 'https://www.youtube.com/embed/G8GEpK7YDl4', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('OMG', 'New jeans', 215, 'https://www.youtube.com/watch?v=jT0Lh-N3TSg', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Butter', '방탄소년단', 165, 'https://www.youtube.com/embed/Uz0PppyT7Cc', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('파랑 (Blue Wave)', 'NCT DREAM', 191, 'https://www.youtube.com/embed/ZkLK4hUqqas', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Take Two', '방탄소년단', 230, 'https://www.youtube.com/embed/3UE-vpej_VI', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Like We Just Met', 'NCT DREAM', 210, 'https://www.youtube.com/embed/eA9pwL-8wJw', now());
+INSERT INTO song (title, singer, length, video_url, created_at)
+VALUES ('Yogurt Shake', 'NCT DREAM', 218, 'https://www.youtube.com/embed/IUs7tOzHVJw', now());
