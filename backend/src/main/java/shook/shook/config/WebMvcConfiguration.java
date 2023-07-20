3ab68833d2a948e7a5b995e76087b9735452d333
splitube <voicesplit@daum.net>
2023-07-20 15:51:52 +0900

    chore: 패키지 이동

--- backend/src/main/java/shook/shook/config/WebMvcConfiguration.java
+++ backend/src/main/java/shook/shook/config/WebMvcConfiguration.java
@@ -1,4 +1,4 @@
-package shook.shook.part.config;
+package shook.shook.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
