1eb107b6737c9b5173283c4d8dc878b36bb7eec1
ukkodeveloper <ukkodeveloper@gmail.com>
2023-07-27 20:40:03 +0900

    Feat/#120 .env 환경 변수 설정 및 MSW 설정 (#132)
    * config: dotenv설치 및 환경변수 설정
    
    * config: msw 라이브러리 설치 및 적용
    
    * refactor: msw 동적 import 로직 간결하게 변경
    
    * config: package-lock 업데이트 및 script에 NODE_ENV 환경변수
    
    * refactor: DefinePlugin common에서 공통적으로 적용
    
    * feat: msw 파일 구조 변경 및 handler 실패시에도 response
    
    * config: package-lock.json 최신화
    
    ---------
    
    Co-authored-by: cruelladevil <dev.timetravel@gmail.com>

---
+++ frontend/src/mocks/handlers/songsHandlers.ts
@@ -0,0 +1,30 @@
+import { rest } from 'msw';
+import songs from '../fixtures/songs.json';
+
+const { BASE_URL } = process.env;
+
+export const songsHandlers = [
+  rest.get(`${BASE_URL}/songs/:songId`, (req, res, ctx) => {
+    const { songId } = req.params;
+
+    const song = songs.find((song) => song.id == Number(songId));
+
+    if (!song) {
+      return res(
+        ctx.status(404),
+        ctx.json({ message: `id:${songId}에 해당되는 노래가 없습니다.` })
+      );
+    }
+
+    return res(
+      ctx.json({
+        id: song.id,
+        title: song.title,
+        singer: song.singer,
+        videoLength: song.length,
+        songVideoUrl: song.video_url,
+        killingParts: [],
+      })
+    );
+  }),
+];
