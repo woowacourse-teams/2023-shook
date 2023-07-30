5cf14eadd58109e69646a3ffd43d5636e3827cd1
seokhwan-an <asok369258@naver.com>
2023-07-30 19:47:06 +0900

    refactor: 어노테이션 수정
    세부사항
    - MemberService transactional 어노테이션 추가
    - GoogleProvider componenet 어노테이션 service 어노테이션으로 수정

--- backend/src/main/java/shook/shook/auth/jwt/application/TokenProvider.java
+++ backend/src/main/java/shook/shook/auth/jwt/application/TokenProvider.java
@@ -11,10 +11,10 @@ import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import shook.shook.auth.jwt.exception.TokenException;
 
-@Component
+@Service
 public class TokenProvider {
 
     private final long accessTokenValidTime;
--- backend/src/main/java/shook/shook/member/application/MemberService.java
+++ backend/src/main/java/shook/shook/member/application/MemberService.java
@@ -3,6 +3,7 @@ package shook.shook.member.application;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import shook.shook.member.application.dto.MemberRegisterRequest;
 import shook.shook.member.domain.Email;
 import shook.shook.member.domain.Member;
@@ -10,11 +11,13 @@ import shook.shook.member.domain.repository.MemberRepository;
 import shook.shook.member.exception.MemberException;
 
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional
     public Member register(final MemberRegisterRequest memberRegisterRequest) {
         final Optional<Member> member = findByEmail(new Email(memberRegisterRequest.getEmail()));
 
