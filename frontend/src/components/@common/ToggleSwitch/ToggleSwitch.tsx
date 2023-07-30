899460adc8348b29872a741fcf48308a95c1318f
ukkodeveloper <ukkodeveloper@gmail.com>
2023-07-30 21:17:50 +0900

    feat: toggleSwitch 컴포넌트 스타일 분리

--- frontend/src/components/@common/ToggleSwitch/ToggleSwitch.tsx
+++ frontend/src/components/@common/ToggleSwitch/ToggleSwitch.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
+import { SwitchInput, SwitchSlider, SwitchWrapper } from './ToggleSwitch.style';
 
 interface ToggleSwitchProps {
   off: () => void;
@@ -24,49 +24,3 @@ const ToggleSwitch = ({ on, off, defaultToggle = false }: ToggleSwitchProps) => 
 };
 
 export default ToggleSwitch;
-
-const SwitchWrapper = styled.label`
-  position: relative;
-  display: inline-block;
-  width: 40px;
-  height: 20px;
-`;
-
-const SwitchInput = styled.input`
-  opacity: 0;
-  width: 0;
-  height: 0;
-
-  &:checked + span {
-    background-color: ${({ theme }) => theme.color.primary};
-  }
-
-  &:checked + span:before {
-    transform: translateX(18px);
-  }
-`;
-
-const SwitchSlider = styled.span`
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  border-radius: 32px;
-  background-color: ${({ theme: { color } }) => color.disabledBackground};
-  transition: 0.4s;
-
-  &:before {
-    box-shadow: 1px 1px ${({ theme: { color } }) => color.black};
-    position: absolute;
-    border-radius: 50%;
-    content: '';
-    height: 16px;
-    width: 16px;
-    left: 3px;
-    bottom: 2px;
-    background-color: white;
-    transition: 0.4s;
-  }
-`;
