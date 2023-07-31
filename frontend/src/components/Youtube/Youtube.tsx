502c93adbd67167deeac5798e42b0816798a0c08
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-07-31 23:04:01 +0900

    refactor(Youtube): 컴포넌트 리팩토링
    1. 컴포넌트 내부 불필요한 player 상태 삭제
    2. 사용하지 않는 onStateChange 이벤트 핸들러 삭제
    3. 외부에서 player 상태 및 setter 를 전달받도록 개선

--- frontend/src/components/Youtube/Youtube.tsx
+++ frontend/src/components/Youtube/Youtube.tsx
@@ -1,78 +1,42 @@
 /* eslint-disable react/display-name */
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { styled } from 'styled-components';
 import { loadIFrameApi } from '@/components/Youtube/api/loadIframeApi';
 
 interface YoutubeProps {
   videoId: string;
   start: number;
-
-  onReady: (event: YT.PlayerEvent) => void;
+  videoPlayer: YT.Player | undefined;
+  updatePlayer: ({ target }: YT.PlayerEvent) => void;
 }
 
-const Youtube = ({ videoId, start, onReady }: YoutubeProps) => {
-  const [player, setPlayer] = useState<YT.Player | undefined>();
-
+const Youtube = ({ videoId, start, videoPlayer, updatePlayer }: YoutubeProps) => {
   const createYoutubePlayer = async () => {
     try {
       const YT = await loadIFrameApi();
 
-      setPlayer(
-        new YT.Player('yt-player', {
-          videoId,
-          width: '100%',
-          height: '100%',
-          playerVars: { start, autoplay: 1 },
-          events: {
-            onReady: onPlayerReady,
-            onStateChange: onPlayerStateChange,
-          },
-        })
-      );
+      new YT.Player('yt-player', {
+        videoId,
+        width: '100%',
+        height: '100%',
+        playerVars: { start, autoplay: 1 },
+        events: { onReady: updatePlayer },
+      });
     } catch (error) {
       console.error(error);
-      console.error('[createYoutubePlayer] Youtube Player를 생성하지 못하였습니다.');
-    }
-  };
-
-  const onPlayerReady = (event: YT.PlayerEvent) => onReady(event);
-
-  const onPlayerStateChange = (event: YT.OnStateChangeEvent) => {
-    console.log('[onPlayerStateChange]');
-    console.log(event.target);
-
-    switch (event.data) {
-      case YT.PlayerState.UNSTARTED:
-        console.log('[시작전]');
-        break;
-      case YT.PlayerState.PLAYING:
-        console.log('[재생]');
-        break;
-      case YT.PlayerState.PAUSED:
-        console.log('[멈춤]');
-        break;
-      case YT.PlayerState.ENDED:
-        console.log('[끝]');
-        break;
-      case YT.PlayerState.BUFFERING:
-        console.log('[버퍼링]');
-        break;
-      case YT.PlayerState.CUED:
-        console.log('[큐]');
-        break;
-      default:
-        console.log('[NEVER]');
+      console.error('Youtube Player를 생성하지 못하였습니다.');
     }
   };
 
   useEffect(() => {
     createYoutubePlayer();
-    return () => player?.destroy();
+
+    return () => videoPlayer?.destroy();
   }, [videoId]);
 
   return (
     <YoutubeWrapper>
-      <div id="yt-player" />
+      <YoutubeIframe id="yt-player" />
     </YoutubeWrapper>
   );
 };
@@ -83,3 +47,5 @@ export const YoutubeWrapper = styled.div`
   width: 100%;
   aspect-ratio: auto 16 / 9;
 `;
+
+export const YoutubeIframe = styled.div``;
