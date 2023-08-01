92e5fc72feaa22aee4a8c40ea4dd105ac9034579
cruelladevil <dev.timetravel@gmail.com>
2023-08-01 18:43:33 +0900

    feat: 인기있는 킬링파트 서버응답 msw 구현

--- frontend/src/mocks/handlers/songsHandlers.ts
+++ frontend/src/mocks/handlers/songsHandlers.ts
@@ -1,9 +1,14 @@
 import { rest } from 'msw';
+import popularSongs from '../fixtures/popularSongs.json';
 import songs from '../fixtures/songs.json';
 
 const { BASE_URL } = process.env;
 
 export const songsHandlers = [
+  rest.get(`${BASE_URL}/songs/recommended`, (req, res, ctx) => {
+    return res(ctx.json(popularSongs));
+  }),
+
   rest.get(`${BASE_URL}/songs/:songId`, (req, res, ctx) => {
     const { songId } = req.params;
 
