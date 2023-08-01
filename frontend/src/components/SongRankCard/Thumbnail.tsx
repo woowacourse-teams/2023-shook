fdda8cb60932a966b03181f3d619127f68c75eda
cruelladevil <dev.timetravel@gmail.com>
2023-08-01 19:55:17 +0900

    feat: lazy loading 및 default image 적용

--- frontend/src/components/SongRankCard/Thumbnail.tsx
+++ frontend/src/components/SongRankCard/Thumbnail.tsx
@@ -1,12 +1,22 @@
+import defaultAlbumJacket from '@/assets/icon/album-jacket-default.svg';
 import { Wrapper } from './Thumbnail.style';
-import type { ImgHTMLAttributes } from 'react';
+import type { ImgHTMLAttributes, SyntheticEvent } from 'react';
 
 interface ThumbnailProps extends ImgHTMLAttributes<HTMLImageElement> {}
 
 const Thumbnail = ({ ...props }: ThumbnailProps) => {
+  const insertDefaultJacket = ({ currentTarget }: SyntheticEvent<HTMLImageElement>) => {
+    currentTarget.src = defaultAlbumJacket;
+  };
+
   return (
     <Wrapper>
-      <img {...props} style={{ borderRadius: '8px' }} />
+      <img
+        {...props}
+        style={{ borderRadius: '8px' }}
+        loading="lazy"
+        onError={insertDefaultJacket}
+      />
     </Wrapper>
   );
 };
