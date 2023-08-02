67d9890539f4c4bd73c84b628e07671ea44b13cb
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-02 22:54:52 +0900

    design: 사용하지 않는 코드 삭제 및 모달버튼 간격 조정

--- frontend/src/pages/SongDetailPage.style.ts
+++ frontend/src/pages/SongDetailPage.style.ts
@@ -3,13 +3,8 @@ import { styled } from 'styled-components';
 export const ButtonContainer = styled.div`
   display: flex;
   width: 100%;
+  gap: 16px;
 `;
-
-// export const Spacing = styled.div<{ direction: 'horizontal' | 'vertical'; size: number }>`
-//   flex: none;
-//   width: ${({ direction, size }) => (direction === 'horizontal' ? `${size}px` : undefined)};
-//   height: ${({ direction, size }) => (direction === 'vertical' ? `${size}px` : undefined)};
-// `;
 
 export const Container = styled.section`
   display: flex;
