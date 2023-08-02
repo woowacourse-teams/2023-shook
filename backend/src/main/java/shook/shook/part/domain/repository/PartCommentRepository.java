ee743511ee8e39f26087c22ca6094d11d7cde441
splitube <voicesplit@daum.net>
2023-08-02 13:40:15 +0900

    refactor: Repository 어노테이션 추가

--- backend/src/main/java/shook/shook/part/domain/repository/PartCommentRepository.java
+++ backend/src/main/java/shook/shook/part/domain/repository/PartCommentRepository.java
@@ -1,8 +1,10 @@
 package shook.shook.part.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import shook.shook.part.domain.PartComment;
 
+@Repository
 public interface PartCommentRepository extends JpaRepository<PartComment, Long> {
 
 }
