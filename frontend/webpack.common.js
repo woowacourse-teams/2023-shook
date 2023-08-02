e9de5f03dacea00c61eaee4c0aa4bd4e86b086b5
cruelladevil <dev.timetravel@gmail.com>
2023-08-02 13:03:32 +0900

    config: font를 지원하기 위한 webpack 및 type 설정

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -12,6 +12,10 @@ module.exports = {
         test: /\.(png|svg|jpe?g)$/,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
