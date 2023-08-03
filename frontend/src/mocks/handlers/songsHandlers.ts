b394758e5e6cacd69f1234af03bcc8c8d63aeff2
cruelladevil <dev.timetravel@gmail.com>
2023-08-03 16:51:10 +0900

    test: 댓글 조회 및 등록 msw 적용

--- frontend/src/mocks/handlers/songsHandlers.ts
+++ frontend/src/mocks/handlers/songsHandlers.ts
@@ -4,6 +4,35 @@ import songs from '../fixtures/songs.json';
 const { BASE_URL } = process.env;
 
 export const songsHandlers = [
+  rest.get(`${BASE_URL}/songs/:songId/parts/:partId/comments`, (req, res, ctx) => {
+    const comments = [
+      { id: 1, content: '1번 댓글입니다.', createdAt: '2023-08-01T16:02:13.422Z' },
+      {
+        id: 2,
+        content: '2번 댓글입니다. 200자 입니다. '.repeat(10),
+        createdAt: '2023-08-02T16:02:13.422Z',
+      },
+      { id: 3, content: '3번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
+      { id: 4, content: '4번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
+      { id: 5, content: '5번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
+      { id: 6, content: '6번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
+      { id: 7, content: '7번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
+      { id: 8, content: '8번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
+      { id: 9, content: '9번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
+    ];
+
+    return res(ctx.json(comments));
+  }),
+
+  rest.post(`${BASE_URL}/songs/:songId/parts/:partId/comments`, async (req, res, ctx) => {
+    const { songId, partId } = req.params;
+    const content = await req.json();
+
+    console.log(songId, partId, JSON.parse(content));
+
+    return res(ctx.status(201));
+  }),
+
   rest.get(`${BASE_URL}/songs/:songId`, (req, res, ctx) => {
     const { songId } = req.params;
 
