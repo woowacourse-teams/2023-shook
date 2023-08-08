e6df3762ca173ae9f6484e23ca5279ba321a2276
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-09 00:31:52 +0900

    test: 시간 유틸 테스트케이스 추가
    3개 함수에 대한 테스트 케이스 추가 및 검증

--- frontend/src/shared/utils/convertTime.test.ts
+++ frontend/src/shared/utils/convertTime.test.ts
@@ -1,4 +1,10 @@
-import { minSecToSeconds, secondsToMinSec } from './convertTime';
+import {
+  addLeadingZeroForTimeFormat,
+  getTimeFormatText,
+  isValidateTimeFormatLength,
+  minSecToSeconds,
+  secondsToMinSec,
+} from './convertTime';
 
 describe('minSecToSeconds: 분/초 정보를 바탕으로 초 단위로 변환', () => {
   test('2분 30초를 받으면 150초를 반환한다.', () => {
@@ -17,5 +23,83 @@ describe('secondsToMinSec:', () => {
 
   test('45초를 받으면 0분 45초를 반환한다.', () => {
     expect(secondsToMinSec(45)).toEqual([0, 45]);
+  });
+});
+
+describe('isValidateTimeFormatLength 함수 검증 테스트', () => {
+  test.each(['1', '22', '33'])('길이 2 이하의 string type 인자라면 true를 반환한다.', (args) => {
+    expect(isValidateTimeFormatLength(args)).toBe(true);
+  });
+
+  test.each(['101', '111', '121'])(
+    '길이 2 초과의 string type 인자라면 false를 반환한다.',
+    (args) => {
+      expect(isValidateTimeFormatLength(args)).toBe(false);
+    }
+  );
+
+  test.each([1, 22, 33])('길이 2 이하의 number type 인자라면 true를 반환한다.', (args) => {
+    expect(isValidateTimeFormatLength(args)).toBe(true);
+  });
+
+  test.each([101, 111, 121])('길이 2 초과의 number type 인자라면 false를 반환한다.', (args) => {
+    expect(isValidateTimeFormatLength(args)).toBe(false);
+  });
+});
+
+describe('addLeadingZeroForTimeFormat 함수 검증 테스트', () => {
+  test('길이 2 미만의 string type 인자라면 인자의 맨 앞에 0을 붙여 문자열로 반환한다.', () => {
+    const result = addLeadingZeroForTimeFormat('1');
+    const expected = '01';
+
+    expect(result).toBe(expected);
+  });
+
+  test('길이 2의 string type 인자라면 그대로 반환한다.', () => {
+    const result = addLeadingZeroForTimeFormat('12');
+    const expected = '12';
+
+    expect(result).toBe(expected);
+  });
+
+  test('길이 2 미만의 number type 인자라면 맨 앞에 0을 붙여 문자열로 반환한다.', () => {
+    const result = addLeadingZeroForTimeFormat(2);
+    const expected = '02';
+
+    expect(result).toBe(expected);
+  });
+
+  test('길이 2의 number type 인자라면 그대로 문자열로 반환한다.', () => {
+    const result = addLeadingZeroForTimeFormat(12);
+    const expected = '12';
+
+    expect(result).toBe(expected);
+  });
+
+  test('길이 2 초과의 string type 인자라면 에러가 발생한다.', () => {
+    expect(() => {
+      addLeadingZeroForTimeFormat('119');
+    }).toThrow();
+  });
+
+  test('길이 2 초과의 number type 인자라면 에러가 발생한다.', () => {
+    expect(() => {
+      addLeadingZeroForTimeFormat(119);
+    }).toThrow();
+  });
+});
+
+describe('getTimeFormatText 함수 검증 테스트', () => {
+  test('길이 2 이하의 string or number type 인자에 길이가 2가 되도록 앞에서부터 0을 붙이고, 각 요소 사이에 : 구분자를 붙여 시간 포멧 텍스트로 return한다.', () => {
+    const result = getTimeFormatText('1', 22);
+    const expected = '01:22';
+
+    expect(result).toBe(expected);
+  });
+
+  test('인자 중 하나라도 길이가 2를 초과하면 에러가 발생한다.', () => {
+    expect(() => {
+      getTimeFormatText('1', 222);
+    }).toThrow();
   });
 });
