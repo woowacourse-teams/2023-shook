4de6b6db2a457cc708ab5fef550adf441d7625fd
cruelladevil <dev.timetravel@gmail.com>
2023-08-10 17:23:46 +0900

    test: 파라미터 변경으로인한 테스트 수정
    - related e65cf10

--- frontend/src/shared/utils/convertTime.test.ts
+++ frontend/src/shared/utils/convertTime.test.ts
@@ -8,21 +8,21 @@ import {
 
 describe('minSecToSeconds: 분/초 정보를 바탕으로 초 단위로 변환', () => {
   test('2분 30초를 받으면 150초를 반환한다.', () => {
-    expect(minSecToSeconds([2, 30])).toBe(150);
+    expect(minSecToSeconds({ minute: 2, second: 30 })).toBe(150);
   });
 
   test('0분 45초를 받으면 45초를 반환한다.', () => {
-    expect(minSecToSeconds([0, 45])).toBe(45);
+    expect(minSecToSeconds({ minute: 0, second: 45 })).toBe(45);
   });
 });
 
 describe('secondsToMinSec:', () => {
   test('150초를 받으면 2분 30초를 반환한다.', () => {
-    expect(secondsToMinSec(150)).toEqual([2, 30]);
+    expect(secondsToMinSec(150)).toEqual({ minute: 2, second: 30 });
   });
 
   test('45초를 받으면 0분 45초를 반환한다.', () => {
-    expect(secondsToMinSec(45)).toEqual([0, 45]);
+    expect(secondsToMinSec(45)).toEqual({ minute: 0, second: 45 });
   });
 });
 
