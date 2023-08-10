b53fe1e1b0d8022a30a0cf8cbb2c9b652e8d2ea1
ukkodeveloper <ukkodeveloper@gmail.com>
2023-08-10 23:12:36 +0900

    feat: 캐러셀 컴포넌트 함수 분리 및 스타일 수정

--- frontend/src/features/songs/components/CollectionCarousel.tsx
+++ frontend/src/features/songs/components/CollectionCarousel.tsx
@@ -1,43 +1,54 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
-import Spacing from '@/shared/components/Spacing';
+import type { PropsWithChildren } from 'react';
+import type React from 'react';
 
-const Carousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const ref = useRef<HTMLDivElement>(null);
+const getChildrenLength = (children: React.ReactNode) => {
+  let itemLength = 1;
+
+  if (Array.isArray(children)) {
+    itemLength = children.length;
+  }
+
+  return itemLength;
+};
+
+interface CarouselProps {
+  children: React.ReactNode;
+}
+
+const CollectionCarousel = ({ children }: CarouselProps) => {
+  const [currentItem, setCurrentItem] = useState(0);
+  const itemLength = getChildrenLength(children);
+
   const handleScroll: React.UIEventHandler<HTMLDivElement> = (e) => {
     const { scrollLeft, offsetWidth } = e.target as HTMLDivElement;
-    console.log(offsetWidth);
-    console.log(scrollLeft);
-
     const index = Math.round(scrollLeft / offsetWidth);
-    console.log(index);
-    setCurrentIndex(index);
+
+    setCurrentItem(index);
   };
 
   return (
     <>
       <Wrapper>
-        <CarouselWrapper onScroll={handleScroll}>
-          {Array.from({ length: 10 }, (_, idx) => {
-            return (
-              <Item ref={ref} key={idx}>
-                {idx}
-              </Item>
-            );
-          })}
-        </CarouselWrapper>
-        <Spacing direction="vertical" size={8} />
+        <CarouselWrapper onScroll={handleScroll}>{children}</CarouselWrapper>
         <IndicatorWrapper>
-          {Array.from({ length: 10 }, (_, idx) => (
-            <Dot key={idx} isActive={idx === currentIndex} />
+          {Array.from({ length: itemLength }, (_, idx) => (
+            <Dot key={idx} isActive={idx === currentItem} />
           ))}
         </IndicatorWrapper>
       </Wrapper>
     </>
   );
 };
-export default Carousel;
+
+const CarouselItem = ({ children }: PropsWithChildren) => {
+  return <Item>{children}</Item>;
+};
+
+CollectionCarousel.item = CarouselItem;
+
+export default CollectionCarousel;
 
 const Wrapper = styled.div`
   width: 100%;
@@ -48,6 +59,8 @@ const CarouselWrapper = styled.div`
   display: flex;
   will-change: transform;
   column-gap: 16px;
+  margin-bottom: 16px;
+  width: 100%;
 
   overflow-x: scroll;
   scroll-behavior: smooth;
@@ -64,30 +77,11 @@ const CarouselWrapper = styled.div`
   }
 `;
 
-const Item = styled.div`
-  flex: 0 0 auto;
-  z-index: -1;
-  width: 100%;
-
-  height: 140px;
-  color: black;
-
-  background-color: white;
-  border-radius: 8px;
-
-  font-size: 24px;
-  transition: 1s all ease;
-`;
-
 const IndicatorWrapper = styled.div`
   display: flex;
   justify-content: center;
-
-  z-index: 1;
-
   background-color: transparent;
-  padding: 4px;
-  border-radius: 4px;
+  column-gap: 8px;
 `;
 
 const Dot = styled.div<{ isActive: boolean }>`
@@ -95,6 +89,24 @@ const Dot = styled.div<{ isActive: boolean }>`
   height: 8px;
   border-radius: 50%;
   background-color: ${({ isActive, theme: { color } }) =>
-    isActive ? color.primary : color.disabledBackground};
-  margin: 0 3px;
+    isActive ? color.primary : color.secondary};
+`;
+
+const Item = styled.li`
+  flex: 0 0 auto;
+  z-index: -1;
+  width: 100%;
+  list-style-type: none;
+
+  height: 140px;
+  color: ${({ theme }) => theme.color.white};
+
+  background-color: ${({ theme }) => theme.color.secondary};
+  border-radius: 8px;
+
+  transition: 1s all ease;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  padding: 16px;
 `;
