54ddad69d91c371a62d5a8ae6ba94bd6b74f8599
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-10 17:59:43 +0900

    test: 킬링파트 트랙 스토리 추가

--- frontend/src/features/songs/components/KillingPartTrack.stories.tsx
+++ frontend/src/features/songs/components/KillingPartTrack.stories.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import KillingPartTrack from './KillingPartTrack';
+import type { KillingPart } from '@/shared/types/song';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
@@ -9,6 +11,35 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof KillingPartTrack>;
 
+const killingPart: KillingPart = {
+  exist: true,
+  id: 1,
+  rank: 1,
+  voteCount: 0,
+  start: 70,
+  end: 80,
+  partVideoUrl: 'https://youtu.be/ArmDp-zijuc?start=105&end=115',
+  likeCount: 12,
+};
+
+const KillingPartTrackWithHook = () => {
+  const [nowPlayingTrack, setNowPlayingTrack] = useState(-1);
+
+  const changePlayingTrack: React.ChangeEventHandler<HTMLInputElement> = ({ currentTarget }) => {
+    const newTrack = Number(currentTarget.value);
+
+    setNowPlayingTrack(newTrack);
+  };
+
+  return (
+    <KillingPartTrack
+      killingPart={killingPart}
+      nowPlayingTrack={nowPlayingTrack}
+      changePlayingTrack={changePlayingTrack}
+    />
+  );
+};
+
 export const Default: Story = {
-  render: () => <KillingPartTrack />,
+  render: () => <KillingPartTrackWithHook />,
 };
