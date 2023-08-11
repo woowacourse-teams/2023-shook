797e175ec8645eea68120b7712e4af0a5ce97347
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-11 21:56:03 +0900

    refactor: 썸네일 컴포넌트 사이즈 props 추가

--- frontend/src/features/songs/components/Thumbnail.tsx
+++ frontend/src/features/songs/components/Thumbnail.tsx
@@ -1,16 +1,18 @@
-import { styled } from 'styled-components';
+import { css, styled } from 'styled-components';
 import defaultAlbumJacket from '@/assets/icon/album-jacket-default.svg';
 import type { ImgHTMLAttributes, SyntheticEvent } from 'react';
 
-interface ThumbnailProps extends ImgHTMLAttributes<HTMLImageElement> {}
+interface ThumbnailProps extends ImgHTMLAttributes<HTMLImageElement> {
+  size?: Size;
+}
 
-const Thumbnail = ({ ...props }: ThumbnailProps) => {
+const Thumbnail = ({ size = 'lg', ...props }: ThumbnailProps) => {
   const insertDefaultJacket = ({ currentTarget }: SyntheticEvent<HTMLImageElement>) => {
     currentTarget.src = defaultAlbumJacket;
   };
 
   return (
-    <Wrapper>
+    <Wrapper size={size}>
       <img {...props} alt="노래 앨범" aria-hidden loading="lazy" onError={insertDefaultJacket} />
     </Wrapper>
   );
@@ -18,9 +20,25 @@ const Thumbnail = ({ ...props }: ThumbnailProps) => {
 
 export default Thumbnail;
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ size: Size }>`
   overflow: hidden;
-  width: 70px;
-  height: 70px;
+  ${({ size }) => SIZE[size]};
   border-radius: 8px;
 `;
+
+const md = css`
+  width: 60px;
+  height: 60px;
+`;
+
+const lg = css`
+  width: 70px;
+  height: 70px;
+`;
+
+const SIZE = {
+  md,
+  lg,
+} as const;
+
+type Size = keyof typeof SIZE;
