91717d64c0a856b604e8e5b3cd7f180307238eb9
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-13 01:45:03 +0900

    refactor: 플레이어 상태 별 변수 분리 및 토글 함수 추가

--- frontend/src/features/youtube/components/VideoPlayerProvider.tsx
+++ frontend/src/features/youtube/components/VideoPlayerProvider.tsx
@@ -4,8 +4,7 @@ import type { PropsWithChildren } from 'react';
 interface VideoPlayerContextProps {
   videoPlayer: React.MutableRefObject<YT.Player | undefined>;
   play: (start: number) => void;
-  pause: () => void;
-  currentPlayTime: number;
+  togglePlayerPauseAndResume: () => void;
 }
 
 export const VideoPlayerContext = createContext<VideoPlayerContextProps | null>(null);
@@ -13,26 +12,35 @@ export const VideoPlayerContext = createContext<VideoPlayerContextProps | null>(
 export const VideoPlayerProvider = ({ children }: PropsWithChildren) => {
   const videoPlayer = useRef<YT.Player>();
 
+  const pause = () => videoPlayer.current?.pauseVideo();
+  const resume = () => videoPlayer.current?.playVideo();
+
   const play = (start: number) => {
-    if (videoPlayer.current?.getPlayerState() === 2) {
-      videoPlayer.current?.playVideo();
+    const isPause = videoPlayer.current?.getPlayerState() === 2;
+    if (isPause) {
+      resume();
     }
 
     videoPlayer.current?.seekTo(start, true);
   };
 
-  const pause = () => {
+  const togglePlayerPauseAndResume = () => {
     const isPlaying = videoPlayer.current?.getPlayerState() === 1;
+    const isPaused = videoPlayer.current?.getPlayerState() === 2;
 
     if (isPlaying) {
-      videoPlayer.current?.pauseVideo();
+      pause();
+      return;
+    }
+
+    if (isPaused) {
+      resume();
+      return;
     }
   };
 
-  const currentPlayTime = videoPlayer.current?.getCurrentTime() ?? 0;
-
   return (
-    <VideoPlayerContext.Provider value={{ videoPlayer, play, pause, currentPlayTime }}>
+    <VideoPlayerContext.Provider value={{ videoPlayer, play, togglePlayerPauseAndResume }}>
       {children}
     </VideoPlayerContext.Provider>
   );
