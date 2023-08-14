9aac9fb605540989683d5a92c3fb050a616f60c0
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-14 13:39:16 +0900

    refactor: 플레이어 프로바이더의 함수에 callback 적용

--- frontend/src/features/youtube/components/VideoPlayerProvider.tsx
+++ frontend/src/features/youtube/components/VideoPlayerProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from 'react';
+import { createContext, useCallback, useRef, useState } from 'react';
 import type { PropsWithChildren } from 'react';
 
 interface VideoPlayerContextProps {
@@ -15,19 +15,22 @@ export const VideoPlayerProvider = ({ children }: PropsWithChildren) => {
   const videoPlayer = useRef<YT.Player>();
   const [playerState, setPlayerState] = useState<YT.PlayerState>();
 
-  const pause = () => videoPlayer.current?.pauseVideo();
-  const play = () => videoPlayer.current?.playVideo();
+  const pause = useCallback(() => videoPlayer.current?.pauseVideo(), []);
+  const play = useCallback(() => videoPlayer.current?.playVideo(), []);
 
-  const seekTo = (start: number) => {
-    videoPlayer.current?.seekTo(start, true);
-    play();
-  };
+  const seekTo = useCallback(
+    (start: number) => {
+      videoPlayer.current?.seekTo(start, true);
+      play();
+    },
+    [play]
+  );
 
-  const updatePlayerState: YT.PlayerEventHandler<YT.PlayerEvent> = ({ target }) => {
+  const updatePlayerState: YT.PlayerEventHandler<YT.PlayerEvent> = useCallback(({ target }) => {
     const state = target.getPlayerState();
 
     setPlayerState(state);
-  };
+  }, []);
 
   return (
     <VideoPlayerContext.Provider
