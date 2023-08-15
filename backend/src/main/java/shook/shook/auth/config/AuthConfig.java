9fdc3a324e272444ddf6818f90694b45a02fd21a
seokhwan-an <asok369258@naver.com>
2023-08-15 17:17:43 +0900

    feat: 인터셉터 요청 method 별로 구분하는 기능 추가

--- backend/src/main/java/shook/shook/auth/config/AuthConfig.java
+++ backend/src/main/java/shook/shook/auth/config/AuthConfig.java
@@ -3,16 +3,47 @@ package shook.shook.auth.config;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import shook.shook.auth.ui.argumentresolver.AuthArgumentResolver;
+import shook.shook.auth.ui.interceptor.LoginCheckerInterceptor;
+import shook.shook.auth.ui.interceptor.PathMatcherInterceptor;
+import shook.shook.auth.ui.interceptor.PathMethod;
+import shook.shook.auth.ui.interceptor.TokenInterceptor;
 
 @Configuration
 public class AuthConfig implements WebMvcConfigurer {
 
     private final AuthArgumentResolver authArgumentResolver;
+    private final LoginCheckerInterceptor loginCheckerInterceptor;
+    private final TokenInterceptor tokenInterceptor;
 
-    public AuthConfig(final AuthArgumentResolver authArgumentResolver) {
+    public AuthConfig(final AuthArgumentResolver authArgumentResolver,
+        final LoginCheckerInterceptor loginCheckerInterceptor,
+        final TokenInterceptor tokenInterceptor
+    ) {
         this.authArgumentResolver = authArgumentResolver;
+        this.loginCheckerInterceptor = loginCheckerInterceptor;
+        this.tokenInterceptor = tokenInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(loginCheckerInterceptor());
+        registry.addInterceptor(tokenInterceptor());
+    }
+
+    private HandlerInterceptor loginCheckerInterceptor() {
+        return new PathMatcherInterceptor(loginCheckerInterceptor)
+            .includePathPattern("/songs/*", PathMethod.GET);
+    }
+
+    private HandlerInterceptor tokenInterceptor() {
+        return new PathMatcherInterceptor(tokenInterceptor)
+            .includePathPattern("/songs/*/parts/*/likes", PathMethod.PUT)
+            .includePathPattern("/voting-songs/*/parts", PathMethod.POST)
+            .includePathPattern("/songs/*/parts/*/comments", PathMethod.POST);
     }
 
     @Override
