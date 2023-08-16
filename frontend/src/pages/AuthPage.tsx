fdfef12428c75de403d1064e3c141d7fc9cbebcb
ukkodeveloper <ukkodeveloper@gmail.com>
2023-08-16 20:42:21 +0900

    style: todo 주석 추가

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -14,6 +14,9 @@ const AuthPage = () => {
       method: 'get',
     });
     const data = (await response.json()) as AccessTokenResponse;
+    // accessToken을 바탕으로 userId와 userNickname을 추출할 수 있다.
+    // accessToken은 localStorage에 저장한다.
+    // userId와 userNickname은 전역으로 저장한다.
     const { accessToken } = data;
     localStorage.setItem('userToken', accessToken);
   };
