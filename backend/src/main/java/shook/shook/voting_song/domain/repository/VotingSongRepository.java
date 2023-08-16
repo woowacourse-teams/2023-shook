6f005d2aa98fd3b2fc4aa711d6e3d2e432137f2b
splitCoding <voicesplit@daum.net>
2023-08-17 05:39:11 +0900

    refactor: 투표 중인 노래 조회 기능 리팩토링
    빠른 진행을 PR 대신 주석으로 리뷰내용을 작성하고 suggestion 코드 작성

--- backend/src/main/java/shook/shook/voting_song/domain/repository/VotingSongRepository.java
+++ backend/src/main/java/shook/shook/voting_song/domain/repository/VotingSongRepository.java
@@ -2,7 +2,6 @@ package shook.shook.voting_song.domain.repository;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import shook.shook.song.domain.SongTitle;
@@ -13,7 +12,5 @@ public interface VotingSongRepository extends JpaRepository<VotingSong, Long> {
 
     Optional<VotingSong> findByTitle(final SongTitle title);
 
-    List<VotingSong> findByIdLessThanOrderByIdDesc(final Long id, final Pageable page);
-
-    List<VotingSong> findByIdGreaterThanOrderByIdAsc(final Long id, final Pageable page);
+    List<VotingSong> findByIdGreaterThanEqualAndIdLessThanEqual(final Long start, final Long end);
 }
