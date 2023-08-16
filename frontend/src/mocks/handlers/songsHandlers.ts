0188ba55fba917fd4a2f600b9495950e1fad88b1
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-16 21:45:01 +0900

    refactor: fixture data 분리 및 id 정렬

--- frontend/src/mocks/handlers/songsHandlers.ts
+++ frontend/src/mocks/handlers/songsHandlers.ts
@@ -1,4 +1,5 @@
 import { rest } from 'msw';
+import comments from '../fixtures/comments.json';
 import popularSongs from '../fixtures/popularSongs.json';
 import songsTemp from '../fixtures/songsTemp.json';
 import type { KillingPartPostRequest } from '@/shared/types/killingPart';
@@ -11,23 +12,7 @@ export const songsHandlers = [
   }),
 
   rest.get(`${BASE_URL}/songs/:songId/parts/:partId/comments`, (req, res, ctx) => {
-    const comments = [
-      { id: 1, content: '1번 댓글입니다.', createdAt: '2023-08-01T16:02:13.422Z' },
-      {
-        id: 2,
-        content: '2번 댓글입니다. 200자 입니다. '.repeat(10),
-        createdAt: '2023-08-02T16:02:13.422Z',
-      },
-      { id: 3, content: '3번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
-      { id: 4, content: '4번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
-      { id: 5, content: '5번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
-      { id: 6, content: '6번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
-      { id: 7, content: '7번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
-      { id: 8, content: '8번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
-      { id: 9, content: '9번 댓글입니다.', createdAt: '2023-08-02T16:02:13.422Z' },
-    ];
-
-    return res(ctx.json(comments));
+    return res(ctx.status(200), ctx.json(comments));
   }),
 
   rest.post(`${BASE_URL}/songs/:songId/parts/:partId/comments`, async (req, res, ctx) => {
