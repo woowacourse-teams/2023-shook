aa198bd5711eb3b304ba85de3e51e8794e750939
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-08-16 21:57:40 +0900

    Feat/#269 노래 정보 조회 API 구현 (스와이프용) (#278)
    * feat: member equals, hashcode 추가
    
    * refactor: KillingPartLike 에서 Member를 eager loading 하도록 변경
    
    * feat: 스와이프를 위한 이전, 이후 노래 조회 API 추가
    
    * test: updatedAt, createdAt 을 비교 대상에서 제외
    
    * test: updatedAt, createdAt 을 비교 대상에서 제외
    
    * refactor: MemberInfo 적용
    
    * test: 테스트 설명 추가
    
    * refactor: url 수정

--- backend/src/main/java/shook/shook/song/application/SongService.java
+++ backend/src/main/java/shook/shook/song/application/SongService.java
@@ -2,14 +2,17 @@ package shook.shook.song.application;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shook.shook.auth.ui.Authority;
+import shook.shook.auth.ui.argumentresolver.MemberInfo;
 import shook.shook.member.domain.Member;
 import shook.shook.member.domain.repository.MemberRepository;
+import shook.shook.member.exception.MemberException.MemberNotExistException;
 import shook.shook.song.application.dto.SongResponse;
+import shook.shook.song.application.dto.SongSwipeResponse;
 import shook.shook.song.application.dto.SongWithKillingPartsRegisterRequest;
 import shook.shook.song.application.killingpart.dto.HighLikedSongResponse;
 import shook.shook.song.domain.Song;
@@ -22,6 +25,9 @@ import shook.shook.song.exception.SongException;
 @Transactional(readOnly = true)
 @Service
 public class SongService {
+
+    private static final int AFTER_SONGS_COUNT = 10;
+    private static final int BEFORE_SONGS_COUNT = 10;
 
     private final SongRepository songRepository;
     private final KillingPartRepository killingPartRepository;
@@ -56,17 +62,96 @@ public class SongService {
             ).toList();
     }
 
-    public SongResponse findByIdAndMemberId(final Long songId, final Long memberId) {
-        final Song song = songRepository.findById(songId)
-            .orElseThrow(SongException.SongNotExistException::new);
+    public SongSwipeResponse findSongByIdForFirstSwipe(
+        final Long songId,
+        final MemberInfo memberInfo
+    ) {
+        final Song currentSong = findSongById(songId);
 
-        if (Objects.isNull(memberId)) {
-            return SongResponse.fromUnauthorizedUser(song);
+        final List<Song> beforeSongs = findBeforeSongs(currentSong);
+        final List<Song> afterSongs = findAfterSongs(currentSong);
+
+        return convertToSongSwipeResponse(memberInfo, currentSong, beforeSongs, afterSongs);
+    }
+
+    private Song findSongById(final Long songId) {
+        return songRepository.findById(songId)
+            .orElseThrow(SongException.SongNotExistException::new);
+    }
+
+    private List<Song> findBeforeSongs(final Song song) {
+        return songRepository.findSongsWithMoreLikeCountThanSongWithId(
+            song.getId(), PageRequest.of(0, BEFORE_SONGS_COUNT)
+        );
+    }
+
+    private List<Song> findAfterSongs(final Song song) {
+        return songRepository.findSongsWithLessLikeCountThanSongWithId(
+            song.getId(), PageRequest.of(0, AFTER_SONGS_COUNT)
+        );
+    }
+
+    private SongSwipeResponse convertToSongSwipeResponse(
+        final MemberInfo memberInfo,
+        final Song currentSong,
+        final List<Song> beforeSongs, final List<Song> afterSongs
+    ) {
+        final Authority authority = memberInfo.getAuthority();
+
+        if (authority.isAnonymous()) {
+            return SongSwipeResponse.ofUnauthorizedUser(currentSong, beforeSongs, afterSongs);
         }
 
-        final Optional<Member> foundMember = memberRepository.findById(memberId);
+        final Member member = findMemberById(memberInfo.getMemberId());
 
-        return foundMember.map(member -> SongResponse.of(song, member))
-            .orElseGet(() -> SongResponse.fromUnauthorizedUser(song));
+        return SongSwipeResponse.of(member, currentSong, beforeSongs, afterSongs);
+    }
+
+    private Member findMemberById(final Long memberId) {
+        return memberRepository.findById(memberId)
+            .orElseThrow(MemberNotExistException::new);
+    }
+
+    public List<SongResponse> findSongByIdForBeforeSwipe(
+        final Long songId,
+        final MemberInfo memberInfo
+    ) {
+        final Song currentSong = findSongById(songId);
+
+        final List<Song> beforeSongs =
+            findBeforeSongs(currentSong);
+
+        return convertToSongResponses(memberInfo, beforeSongs);
+    }
+
+    private List<SongResponse> convertToSongResponses(
+        final MemberInfo memberInfo,
+        final List<Song> songs
+    ) {
+        final Authority authority = memberInfo.getAuthority();
+
+        if (authority.isAnonymous()) {
+            return songs.stream()
+                .map(SongResponse::fromUnauthorizedUser)
+                .toList();
+        }
+
+        final Member member = findMemberById(memberInfo.getMemberId());
+
+        return songs.stream()
+            .map((song) -> SongResponse.of(song, member))
+            .toList();
+    }
+
+    public List<SongResponse> findSongByIdForAfterSwipe(
+        final Long songId,
+        final MemberInfo memberInfo
+    ) {
+        final Song currentSong = findSongById(songId);
+
+        final List<Song> afterSongs =
+            findAfterSongs(currentSong);
+
+        return convertToSongResponses(memberInfo, afterSongs);
     }
 }
