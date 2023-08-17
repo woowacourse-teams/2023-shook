49301f689b35d0a2df95e6a656a7c9281c359a4a
ukkodeveloper <ukkodeveloper@gmail.com>
2023-08-17 12:59:44 +0900

    fix: googleAuthUrl api 문자열 제거 정규표현식 수정

--- frontend/src/features/auth/constants/googleAuthUrl.ts
+++ frontend/src/features/auth/constants/googleAuthUrl.ts
@@ -1,7 +1,6 @@
 import ROUTE_PATH from '@/shared/constants/path';
 
-const redirectUrl = process.env.BASE_URL?.replace('api/?', '');
-
+const redirectUrl = process.env.BASE_URL?.replace(/api\/?/, '');
 const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?scope=email&response_type=code&redirect_uri=${redirectUrl}${ROUTE_PATH.LOGIN_REDIRECT}&client_id=1008951336382-8o2n6n9u8jbj3sb6fe5jdeha9b6alnqa.apps.googleusercontent.com`;
 
 export default googleAuthUrl;
