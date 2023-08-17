1d5f45bbec011dfb1043f8401a30084106a73ac1
ukkodeveloper <ukkodeveloper@gmail.com>
2023-08-17 11:29:47 +0900

    feat: AuthLayout 구현 및 마이페이지에 적용

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter, Link } from 'react-router-dom';
+import googleAuthUrl from './features/auth/constants/googleAuthUrl';
 import { VideoPlayerProvider } from './features/youtube/components/VideoPlayerProvider';
 import AuthPage from './pages/AuthPage';
 import MainPage from './pages/MainPage';
 import MyPage from './pages/MyPage';
 import PartCollectingPage from './pages/PartCollectingPage';
 import SongDetailPage from './pages/SongDetailPage';
+import AuthLayout from './shared/components/Layout/AuthLayout';
 import Layout from './shared/components/Layout/Layout';
 import ROUTE_PATH from './shared/constants/path';
 
@@ -31,7 +33,11 @@ const router = createBrowserRouter([
       },
       {
         path: `${ROUTE_PATH.MY_PAGE}/:id`,
-        element: <MyPage />,
+        element: (
+          <AuthLayout>
+            <MyPage />
+          </AuthLayout>
+        ),
       },
     ],
   },
@@ -41,13 +47,7 @@ const router = createBrowserRouter([
   },
   {
     path: '/test',
-    element: (
-      <Link
-        to={`https://accounts.google.com/o/oauth2/v2/auth?scope=email&response_type=code&redirect_uri=http://localhost:8080${ROUTE_PATH.LOGIN_REDIRECT}&client_id=1008951336382-8o2n6n9u8jbj3sb6fe5jdeha9b6alnqa.apps.googleusercontent.com`}
-      >
-        사랑해요 코나안~
-      </Link>
-    ),
+    element: <Link to={googleAuthUrl}>로그인 테스트</Link>,
   },
 ]);
 
