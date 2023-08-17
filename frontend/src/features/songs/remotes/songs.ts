672b5b90ae970ff3acb06e9a2a80d246da1e59be
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-17 14:39:43 +0900

    feat: 스와이프 song 데이터 추가요청 api 함수 구현

--- frontend/src/features/songs/remotes/songs.ts
+++ frontend/src/features/songs/remotes/songs.ts
@@ -1,6 +1,14 @@
 import fetcher from '@/shared/remotes';
-import type { SongDetailEntries } from '@/shared/types/song';
+import type { SongDetail, SongDetailEntries } from '@/shared/types/song';
 
 export const getSongDetailEntries = async (songId: number): Promise<SongDetailEntries> => {
   return await fetcher(`/songs/${songId}`, 'GET');
+};
+
+export const getPrevSongDetailEntries = async (songId: number): Promise<SongDetail[]> => {
+  return await fetcher(`/songs/${songId}/prev`, 'GET');
+};
+
+export const getNextSongDetailEntries = async (songId: number): Promise<SongDetail[]> => {
+  return await fetcher(`/songs/${songId}/next`, 'GET');
 };
