f921dd478d6970b0e779fa5fe274c8a2d2e6e47f
splitCoding <voicesplit@daum.net>
2023-08-17 22:47:20 +0900

    refactor: CORS 설정을 추가한다.

--- backend/src/main/java/shook/shook/config/WebMvcConfiguration.java
+++ backend/src/main/java/shook/shook/config/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package shook.shook.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    @Value("${cors.allow-origin}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins(allowedOrigin)
             .allowedMethods("*")
             .allowedHeaders("*");
     }
