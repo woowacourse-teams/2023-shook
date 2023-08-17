8a31fe09a8cc65e626499ec3c7cb306533f1c3e2
seokhwan-an <asok369258@naver.com>
2023-08-18 00:07:34 +0900

    feat: 더미 데이터 추가

--- backend/src/main/resources/dev/data.sql
+++ backend/src/main/resources/dev/data.sql
@@ -1,112 +1,93 @@
 TRUNCATE TABLE song;
 
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Super Shy', 'NewJeans', 200, 'ArmDp-zijuc',
-        'http://i.maniadb.com/images/album/999/999126_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Seven (feat. Latto) - Clean Ver.', '정국', 186, 'UUSbUBYqU_8',
-        'http://i.maniadb.com/images/album/1000/000246_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('퀸카 (Queencard)', '(여자)아이들', 162, 'VOcb6ZHxSjc',
-        'http://i.maniadb.com/images/album/992/992813_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('헤어지자 말해요', '박재정', 244, 'SrQzxD8UFdM',
-        'http://i.maniadb.com/images/album/988/988164_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('I AM', 'IVE (아이브)', 208, 'cU0JrSAyy7o', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('이브, 프시케 그리고 푸른 수염의 아내', 'LE SSERAFIM (르세라핌)', 186,
-        'Ii8L0qEvfC8', 'http://i.maniadb.com/images/album/989/989969_1_f.jpg',
+VALUES ('취중고백', '김민석 (멜로망스)', 258, 'FCrMKhrFH7A',
+        'https://cdnimg.melon.co.kr/cm2/album/images/108/16/959/10816959_20211217144957_500.jpg?c1818ddc493cb2bbb4d268431e6de7b5/melon/resize/282/quality/80/optimize',
         now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 1, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 1, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 1, 3, now());
+
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Spicy', 'aespa', 198, '1kfmWl3o8TE',
-        'http://i.maniadb.com/images/album/990/990970_1_f.jpg', now());
+VALUES ('해요 (2022)', '#안녕', 238, 'P6gV_t70KAk', 'https://cdnimg.melon.co.kr/cm2/album/images/109/75/276/10975276_20220603165713_500.jpg?690c69f1d7581bed46767533175728ff/melon/resize/282/quality/80/optimize', now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 2, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 2, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 2, 3, now());
+
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Steal The Show (From "엘리멘탈")', 'Lauv', 194, 'kUMds6XKtfY',
-        '', now());
+VALUES ('TOMBOY', '(여자)아이들', 174, '0wezH4MAncY', 'https://cdnimg.melon.co.kr/cm2/album/images/108/90/384/10890384_20220314111504_500.jpg?4b9dba7aeba43a4e0042eedb6b9865c1/melon/resize/282/quality/80/optimize', now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 3, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 3, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 3, 3, now());
+
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('사랑은 늘 도망가', '임영웅', 273, 'pBEAzM2TRmE',
-        'http://i.maniadb.com/images/album/918/918794_1_f.jpg', now());
+VALUES ('다정히 내 이름을 부르면', '경서예지, 전건호', 263, 'b_6EfFZyBxY', 'https://cdnimg.melon.co.kr/cm2/album/images/106/10/525/10610525_20210518143433_500.jpg?e8c5aa44ff6608c13fa48eb6a20e81af/melon/resize/282/quality/80/optimize', now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 4, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 4, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 4, 3, now());
+
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('ISTJ', 'NCT DREAM', 186, 'es60T3k-tyM',
-        'http://i.maniadb.com/images/album/998/998384_1_f.jpg', now());
+VALUES ('That''s Hilarious', 'Charlie Puth', 146, 'F3KMndbOhIcㅍ', 'https://cdnimg.melon.co.kr/cm2/album/images/108/44/485/10844485_20221006154824_500.jpg?b752b5ed8fad66b79e2705840630dd94/melon/resize/282/quality/80/optimize', now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 5, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 5, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 5, 3, now());
+
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('모래 알갱이', '임영웅', 221, '3_wOZrzmQ1o',
-        'http://i.maniadb.com/images/album/995/995218_1_f.jpg', now());
+VALUES ('Heaven(2023)', '임재현', 279, 'fPLXgfcyoMc', 'https://cdnimg.melon.co.kr/cm2/album/images/111/54/876/11154876_20230121133335_500.jpg?0ae26bb599c92ddd436282395563596e/melon/resize/282/quality/80/optimize', now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 6, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 6, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 6, 3, now());
+
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('UNFORGIVEN (feat. Nile Rodgers)', 'LE SSERAFIM (르세라핌)', 181,
-        'fzSDGXyGTjg',
-        'http://i.maniadb.com/images/album/989/989969_1_f.jpg', now());
+VALUES ('당신을 만나', '김호중, 송가인', 238, 'kn_j1Ipw4DM', 'https://cdnimg.melon.co.kr/cm2/album/images/111/54/876/11154876_20230121133335_500.jpg?0ae26bb599c92ddd436282395563596e/melon/resize/282/quality/80/optimize', now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 7, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 7, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 7, 3, now());
+
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Kitsch', 'IVE (아이브)', 195, 'r572qh2__-U',
-        'http://i.maniadb.com/images/album/984/984148_1_f.jpg', now());
+VALUES ('잘 지내자, 우리 (여름날 우리 X 로이킴)', '로이킴', 258, 'MbSAeRQl0Xw', 'https://cdnimg.melon.co.kr/cm2/album/images/111/54/876/11154876_20230121133335_500.jpg?0ae26bb599c92ddd436282395563596e/melon/resize/282/quality/80/optimize', now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 8, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 8, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 8, 3, now());
+
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('우리들의 블루스', '임영웅', 207, 'epz-aL5RaLQ',
-        'http://i.maniadb.com/images/album/930/930879_1_f.jpg', now());
+VALUES ('빛이 나는 너에게', '던 (DAWN)', 175, 'wkr3S0hIXLk', 'https://cdnimg.melon.co.kr/cm2/album/images/111/54/876/11154876_20230121133335_500.jpg?0ae26bb599c92ddd436282395563596e/melon/resize/282/quality/80/optimize', now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 9, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 9, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 9, 3, now());
+
 INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Candy', 'NCT DREAM', 220, 'QuaVFoBLQeg',
-        'http://i.maniadb.com/images/album/978/978389_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Hype boy', 'NewJeans', 180, 'T--6HBX2K4g',
-        'http://i.maniadb.com/images/album/946/946945_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('다시 만날 수 있을까', '임영웅', 275, 'VPDRLgfqfSs', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Broken Melodies', 'NCT DREAM', 227, 'EPsh2192sTU',
-        'http://i.maniadb.com/images/album/998/998384_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Still With You', '정국', 239, 'BksBNbTIoPE', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('무지개', '임영웅', 198, 'o8e0Qd2H1qc', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Ditto', 'NewJeans', 187, 'haCpjUXIhrI',
-        'http://i.maniadb.com/images/album/978/978654_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('London Boy', '임영웅', 289, 'ZRDuScdwEbE', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('이제 나만 믿어요', '임영웅', 274, 'y1KXYmMuZZA',
-        'http://i.maniadb.com/images/album_t/80/794/794139_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('아버지', '임영웅', 240, 'dbaiMJOnaB4', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Polaroid', '임영웅', 209, 'PVDxs6GUXSI',
-        'http://i.maniadb.com/images/album_t/80/972/972891_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Dynamite', '방탄소년단', 198, 'KhZ5DCd7m6s',
-        'http://i.maniadb.com/images/album_t/80/816/816954_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('손오공', '세븐틴 (SEVENTEEN)', 200, 'tFPbzfU5XL4',
-        'http://i.maniadb.com/images/album_t/80/988/988481_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('인생찬가', '임영웅', 235, 'cXHduPVrcDQ', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('A bientot', '임영웅', 258, 'sZDDLUB8wQE', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('손이 참 곱던 그대', '임영웅', 197, 'OpZIaI-J0uk', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('사랑해 진짜', '임영웅', 241, 'qkledxNCNfY', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('꽃', '지수', 174, '6zM48_rBFbY',
-        'http://i.maniadb.com/images/album_t/80/984/984469_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('연애편지', '임영웅', 217, 'gSQFZvUuQ3s', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('New jeans', 'New jeans', 109, 'G8GEpK7YDl4',
-        'http://i.maniadb.com/images/album_t/80/999/999126_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('OMG', 'New jeans', 215, 'jT0Lh-N3TSg', '', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Butter', '방탄소년단', 165, 'Uz0PppyT7Cc',
-        'http://i.maniadb.com/images/album_t/80/856/856717_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('파랑 (Blue Wave)', 'NCT DREAM', 191, 'ZkLK4hUqqas',
-        'http://i.maniadb.com/images/album_t/80/998/998384_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Take Two', '방탄소년단', 230, '3UE-vpej_VI',
-        'http://i.maniadb.com/images/album_t/80/997/997196_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Like We Just Met', 'NCT DREAM', 210, 'eA9pwL-8wJw',
-        'http://i.maniadb.com/images/album_t/80/998/998384_1_f.jpg', now());
-INSERT INTO song (title, singer, length, video_id, album_cover_url, created_at)
-VALUES ('Yogurt Shake', 'NCT DREAM', 218, 'IUs7tOzHVJw',
-        'http://i.maniadb.com/images/album_t/80/998/998384_1_f.jpg', now());
+VALUES ('파랑 (Blue Wave)', 'NCT DREAM', 189, 'NhgoqtRhb4g', 'https://cdnimg.melon.co.kr/cm2/album/images/111/54/876/11154876_20230121133335_500.jpg?0ae26bb599c92ddd436282395563596e/melon/resize/282/quality/80/optimize', now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (5, 'SHORT', 10, 10, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (10, 'STANDARD', 10, 5, now());
+INSERT INTO killing_part(start_second, length, song_id, like_count, created_at)
+VALUES (100, 'LONG', 10, 3, now());
