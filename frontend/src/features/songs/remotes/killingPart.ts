b7592badd93b143fa99a40fd438e75a46c826632
ukkodeveloper <ukkodeveloper@gmail.com>
2023-08-17 21:08:03 +0900

    fix: voting-parts post api 변경

--- frontend/src/features/songs/remotes/killingPart.ts
+++ frontend/src/features/songs/remotes/killingPart.ts
@@ -1,9 +1,6 @@
 import fetcher from '@/shared/remotes';
-import type { KillingPartPostRequest, KillingPartPostResponse } from '@/shared/types/killingPart';
+import type { KillingPartPostRequest } from '@/shared/types/killingPart';
 
-export const postKillingPart = async (
-  songId: number,
-  body: KillingPartPostRequest
-): Promise<KillingPartPostResponse> => {
-  return await fetcher<KillingPartPostResponse>(`/songs/${songId}/parts`, 'POST', body);
+export const postKillingPart = async (songId: number, body: KillingPartPostRequest) => {
+  return await fetcher(`/voting-songs/${songId}/parts`, 'POST', body);
 };
