ce52625a4810da9caace45db6b8756f76c9e3bf3
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-18 15:10:45 +0900

    fix: 디자인 핫픽스

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -19,9 +19,9 @@ const MainPage = () => {
   const isEmptyVotingSongs = votingSongs.length === 0;
 
   return (
-    <>
+    <Container>
       <SRHeading>shook 메인 페이지</SRHeading>
-      <Title>현재 수집중인 노래</Title>
+      <Title>현재 킬링파트 등록중인 노래</Title>
       <Spacing direction="vertical" size={16} />
       <CollectionCarousel>
         {isEmptyVotingSongs ? (
@@ -55,11 +55,26 @@ const MainPage = () => {
           </Li>
         ))}
       </PopularSongList>
-    </>
+    </Container>
   );
 };
 
 export default MainPage;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-top: ${({ theme: { headerHeight } }) => headerHeight.desktop};
+
+  @media (max-width: ${({ theme }) => theme.breakPoints.xs}) {
+    padding-top: ${({ theme: { headerHeight } }) => headerHeight.mobile};
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakPoints.xxs}) {
+    padding-top: ${({ theme: { headerHeight } }) => headerHeight.xxs};
+  }
+`;
 
 const Li = styled.li`
   width: 100%;
@@ -91,9 +106,10 @@ const PopularSongList = styled.ol`
 `;
 
 const EmptyMessage = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   width: 100%;
   min-width: 350px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
