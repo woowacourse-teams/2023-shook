e9620eb90e1eb0d9aaabbe87a87e0af8796093fe
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-08-18 10:23:13 +0900

    refactor: 필요 없는 정보 삭제

--- backend/src/main/java/shook/shook/auth/application/dto/TokenPair.java
+++ backend/src/main/java/shook/shook/auth/application/dto/TokenPair.java
@@ -1,17 +1,12 @@
 package shook.shook.auth.application.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Schema(description = "로그인 응답")
 @AllArgsConstructor
 @Getter
 public class TokenPair {
 
-    @Schema(description = "액세스 토큰", example = "asdfjalwkejlfkejagalkwelf")
     private String accessToken;
-
-    @Schema(description = "리프레시 토큰", example = "siehgaleihglawknelfkjeiewofj")
     private String refreshToken;
 }
