7cd1a3fd1d165738c395b507b0d5848a393c3b8e
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-08-26 22:48:50 +0900

    refactor: 사용하지 않는 import문 제거

--- frontend/src/shared/hooks/useExtraFetch.ts
+++ frontend/src/shared/hooks/useExtraFetch.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import type { ErrorResponse } from '@/shared/remotes';
 
 type FetchDirection = 'prev' | 'next';
