8bf24da97e798e2a52605ccfa718ab2a7239aa47
ukkodeveloper <ukkodeveloper@gmail.com>
2023-09-17 18:55:00 +0900

    feat: token storage에 setToken 메서드 추가

--- frontend/src/shared/utils/accessTokenStorage.ts
+++ frontend/src/shared/utils/accessTokenStorage.ts
@@ -34,6 +34,11 @@ const accessTokenStorage = {
   getToken() {
     return localStorage.getItem(ACCESS_TOKEN_KEY);
   },
+
+  setToken(token: string) {
+    localStorage.setItem(ACCESS_TOKEN_KEY, token);
+  },
+
   removeToken() {
     localStorage.removeItem(ACCESS_TOKEN_KEY);
   },
