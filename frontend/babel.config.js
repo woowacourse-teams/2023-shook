d3fe01361b03eb15e08b4ac5dcd47ebbd89067e3
cruelladevil <dev.timetravel@gmail.com>
2023-09-19 15:35:49 +0900

    config: fork-ts-checker-webpack-plugin 적용

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -6,12 +6,17 @@ module.exports = {
         targets: {
           // TODO: 브라우저 지원 범위
           chrome: '51',
-        },
+},
         useBuiltIns: 'entry',
         corejs: '3.31.1',
       },
     ],
-    ['@babel/preset-react'],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic',
+      },
+    ],
     ['@babel/preset-typescript'],
   ],
 };
