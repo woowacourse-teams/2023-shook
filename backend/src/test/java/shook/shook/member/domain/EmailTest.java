84a6fbe1f7048ee2cc84d6443f5db1483e93dfd3
seokhwan-an <asok369258@naver.com>
2023-09-20 00:25:50 +0900

    refactor: email 형식 제약조건 변경하기

--- backend/src/test/java/shook/shook/member/domain/EmailTest.java
+++ backend/src/test/java/shook/shook/member/domain/EmailTest.java
@@ -44,21 +44,4 @@ class EmailTest {
         assertThatThrownBy(() -> new Email(email))
             .isInstanceOf(MemberException.TooLongEmailException.class);
     }
-
-    @DisplayName("이메일의 형태가 유효하지 않으면 예외를 던진다.")
-    @ParameterizedTest(name = "이메일이 \"{0}\" 일 때")
-    @ValueSource(strings = {
-        "shook",
-        "shook!wooteco.com",
-        "shook@wooteco.",
-        "@wooteco.com",
-        "shook@",
-        "shook.com"})
-    void create_fail_wrongEmail(final String email) {
-        //given
-        //when
-        //then
-        assertThatThrownBy(() -> new Email(email))
-            .isInstanceOf(MemberException.InValidEmailFormException.class);
-    }
 }
