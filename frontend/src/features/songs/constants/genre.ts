09037b93ebb82b95df57db0f9b2d4f645318bfb5
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-09-20 14:39:29 +0900

    feat: 장르 type 필드 추가

---
+++ frontend/src/features/songs/constants/genre.ts
@@ -0,0 +1,19 @@
+const GENRE = {
+  DANCE: '댄스',
+  HIPHOP: '힙합',
+  BALLAD: '발라드',
+  POP: '팝',
+  RHYTHM_AND_BLUES: 'R&B/Soul',
+  INDIE: '인디',
+  ROCK_METAL: '락/메탈',
+  TROT: '트로트',
+  FOLK_BLUES: '포크/블루스',
+  JAZZ: '재즈',
+  CLASSIC: '클래식',
+  J_POP: 'J-POP',
+  EDM: 'EDM',
+  ETC: '기타',
+} as const;
+`  `;
+
+export default GENRE;
--- frontend/src/features/songs/types/Song.type.ts
+++ frontend/src/features/songs/types/Song.type.ts
@@ -1,9 +1,14 @@
+import type GENRE from '@/features/songs/constants/genre';
+
+export type Genre = keyof typeof GENRE;
+
 export interface Song {
   id: number;
   title: string;
   singer: string;
   albumCoverUrl: string;
   totalLikeCount: number;
+  genre: Genre;
 }
 
 export interface VotingSong {
