c0847e2a437936d18e319ee76ab881d31bf94ae6
ukkodeveloper <ukkodeveloper@gmail.com>
2023-09-27 10:49:06 +0900

    feat: sendGAEvent 함수 인수별 default값 수정

--- frontend/src/shared/googleAnalytics/sendGAEvent.ts
+++ frontend/src/shared/googleAnalytics/sendGAEvent.ts
@@ -1,10 +1,16 @@
+import { GA_CATEGORIES, GA_MEMBER } from '@/shared/constants/GAEventName';
+
 interface GAProps {
   action: string;
   category?: string;
   memberId?: number;
 }
 
-export const sendGAEvent = ({ action, category, memberId }: GAProps) => {
+export const sendGAEvent = ({
+  action,
+  category = GA_CATEGORIES.NOT_COLLECTING,
+  memberId = GA_MEMBER.NOT_COLLECTING,
+}: GAProps) => {
   if ('gtag' in window) {
     window?.gtag('event', action, {
       event_category: category,
