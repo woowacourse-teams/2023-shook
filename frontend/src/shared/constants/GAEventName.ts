f2805c3a15a602bb56b6df036c483496ef2665db
ukkodeveloper <ukkodeveloper@gmail.com>
2023-09-27 10:48:44 +0900

    feat: GA event 상수화

---
+++ frontend/src/shared/constants/GAEventName.ts
@@ -0,0 +1,18 @@
+export const GA_ACTIONS = {
+  COPY_URL: 'click_copy_part',
+  PLAY: 'click_play_part',
+  LIKE: 'click_like',
+  EDIT_PROFILE: 'click_edit_profile',
+  LOGOUT: 'click_logout',
+};
+
+export const GA_CATEGORIES = {
+  SONG_DETAIL: 'song_playing',
+  MY_PAGE: 'profile',
+  NOT_COLLECTING: 'none',
+};
+
+export const GA_MEMBER = {
+  NOT_LOGGED_IN: -1,
+  NOT_COLLECTING: -2,
+};
