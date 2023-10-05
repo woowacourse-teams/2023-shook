b3342aa86ba49f99d54a153797e01f1bcf00197e
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-06 08:34:50 +0900

    feat: 서치 바 컴포넌트 기본 동작, 마크업 및 스타일링 완료

---
+++ frontend/src/features/artistSearch/components/SearchBar.tsx
@@ -0,0 +1,97 @@
+import { useRef, useState } from 'react';
+import styled from 'styled-components';
+import cancelIcon from '@/assets/icon/cancel.svg';
+import searchIcon from '@/assets/icon/search.svg';
+import Flex from '@/shared/components/Flex/Flex';
+import ResultSheet from './ResultSheet';
+
+const SearchBar = () => {
+  const [search, setSearch] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const openSearchBar: React.MouseEventHandler = () => {
+    setSearch(true);
+  };
+
+  const closeSearchBar: React.FocusEventHandler = () => {
+    setSearch(false);
+  };
+
+  const changeQuery: React.ChangeEventHandler<HTMLInputElement> = ({ currentTarget }) => {
+    setSearchQuery(currentTarget.value);
+  };
+
+  const resetQuery: React.MouseEventHandler = () => {
+    setSearchQuery('');
+    inputRef.current?.focus();
+  };
+
+  const isQueryFilled = searchQuery.length !== 0;
+
+  return (
+    <SearchBox $align="center">
+      {search ? (
+        <>
+          <SearchInput
+            type="text"
+            placeholder="아티스트 검색"
+            autoFocus
+            ref={inputRef}
+            value={searchQuery}
+            onChange={changeQuery}
+            onBlur={closeSearchBar}
+          />
+          {isQueryFilled && <ResetQueryButton type="button" onClick={resetQuery} />}
+          <ResultSheet />
+        </>
+      ) : (
+        <SearchButton type="button" onClick={openSearchBar} />
+      )}
+    </SearchBox>
+  );
+};
+
+export default SearchBar;
+
+const SearchBox = styled(Flex)`
+  height: 30px;
+  padding: 0 10px;
+  background-color: ${({ theme }) => theme.color.black200};
+  border-radius: 16px;
+`;
+
+const SearchButton = styled.button`
+  width: 26px;
+  height: 26px;
+  background: url(${searchIcon}) transparent no-repeat;
+  background-size: contain;
+
+  @media (max-width: ${({ theme }) => theme.breakPoints.md}) {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+const ResetQueryButton = styled.button`
+  width: 26px;
+  height: 26px;
+  background: url(${cancelIcon}) transparent no-repeat;
+  background-size: contain;
+
+  @media (max-width: ${({ theme }) => theme.breakPoints.md}) {
+    width: 20px;
+    height: 20px;
+  }
+`;
+
+const SearchInput = styled.input`
+  padding: 0 10px;
+
+  line-height: 1.6;
+  color: white;
+
+  background-color: ${({ theme }) => theme.color.black200};
+  border: none;
+  outline: none;
+`;
