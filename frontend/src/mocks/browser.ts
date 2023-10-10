9660d05962a0e7218f389c85766d3764bc3f6599
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-11 08:54:32 +0900

    feat: 검색 기능 관련 msw 핸들러 및 fixture 구현

--- frontend/src/mocks/browser.ts
+++ frontend/src/mocks/browser.ts
@@ -1,5 +1,6 @@
 import { setupWorker } from 'msw';
 import { memberHandlers } from './handlers/memberHandlers';
+import { searchHandlers } from './handlers/searchHandlers';
 import { songsHandlers } from './handlers/songsHandlers';
 
-export const worker = setupWorker(...songsHandlers, ...memberHandlers);
+export const worker = setupWorker(...songsHandlers, ...memberHandlers, ...searchHandlers);
---
+++ frontend/src/mocks/handlers/searchHandlers.ts
@@ -0,0 +1,23 @@
+import { rest } from 'msw';
+import searchedArtistPreview from '@/mocks/fixtures/searchedArtistPreview.json';
+
+const { BASE_URL } = process.env;
+
+export const searchHandlers = [
+  rest.get(`${BASE_URL}/artists`, (req, res, ctx) => {
+    const query = req.url.searchParams.get('name') ?? '';
+    const search = req.url.searchParams.get('search');
+    const testQueries = ['악동뮤지션', '악동', '뮤지션'];
+
+    const isPreviewRequest = search === 'artist';
+    const isInTestQueries = testQueries.some(
+      (testQuery) => encodeURIComponent(testQuery) === encodeURIComponent(query)
+    );
+
+    if (isPreviewRequest && isInTestQueries) {
+      return res(ctx.status(200), ctx.json(searchedArtistPreview));
+    }
+
+    return res(ctx.status(200), ctx.json([]));
+  }),
+];
