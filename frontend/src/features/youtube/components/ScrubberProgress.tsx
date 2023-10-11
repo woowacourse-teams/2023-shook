905d31eca906a38a8cd07eea96bbb21c2ff9434c
ukkodeveloper <ukkodeveloper@gmail.com>
2023-10-11 15:28:40 +0900

    feat: progress bar 수정 및 전체 재생 progress bar 구현

--- frontend/src/features/youtube/components/ScrubberProgress.tsx
+++ frontend/src/features/youtube/components/ScrubberProgress.tsx
@@ -1,13 +1,11 @@
 import styled, { keyframes } from 'styled-components';
 
 interface ScrubberProgressProps {
-  prevTime: number;
   interval: number;
-  isPaused: boolean;
 }
 
-const ScrubberProgress = ({ prevTime, interval, isPaused }: ScrubberProgressProps) => {
-  return <PlayingBoxBackground prevTime={prevTime} interval={interval} isPaused={isPaused} />;
+const ScrubberProgress = ({ interval }: ScrubberProgressProps) => {
+  return <PlayingBoxBackground interval={interval} />;
 };
 
 export default ScrubberProgress;
@@ -21,22 +19,72 @@ const fillAnimation = keyframes`
   }
 `;
 
-const PlayingBoxBackground = styled.div<{ prevTime: number; interval: number; isPaused: boolean }>`
+const PlayingBoxBackground = styled.div<{ interval: number }>`
   z-index: 0;
   width: 150px;
   height: 50px;
   border-radius: 5px;
 
-  position: absolute; // 화면에 고정된 위치
-  left: 50%; // 뷰포트의 중앙에 위치
-  transform: translateX(-50%); // 뷰포트 중앙
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
 
   background: linear-gradient(to left, transparent 50%, pink 50%);
   background-size: ${({ interval }) => 200 + (30 - interval)}%;
   transition: 10s linear;
   animation: ${fillAnimation} ${({ interval }) => interval}s linear infinite;
-  animation-play-state: ${({ isPaused }) => (isPaused ? 'paused' : 'running')};
-  animation-delay: ${({ prevTime }) => -prevTime}s;
+
+  pointer-events: none;
+`;
+
+export const ScrubberProgressAllPlaying = () => {
+  return <AllPlayingBackground />;
+};
+
+const animate = keyframes`
+  0%, 100% {
+    clip-path: polygon(
+      0% 45%,
+      16% 44%,
+      33% 50%,
+      54% 60%,
+      70% 61%,
+      84% 59%,
+      100% 52%,
+      100% 100%,
+      0% 100%
+    );
+  }
+
+  50% {
+    clip-path: polygon(
+      0% 60%,
+      15% 65%,
+      34% 66%,
+      51% 62%,
+      67% 50%,
+      84% 45%,
+      100% 46%,
+      100% 100%,
+      0% 100%
+    );
+  }
+`;
+
+const AllPlayingBackground = styled.div`
+  z-index: 0;
+  width: 150px;
+  height: 50px;
+  border-radius: 5px;
+
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+
+  background: linear-gradient(to left, deeppink, pink);
+  transition: 10s linear;
+
+  animation: ${animate} 4s ease-in-out infinite;
 
   pointer-events: none;
 `;
