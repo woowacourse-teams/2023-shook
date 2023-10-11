ef420157753c810358b9e509b651d47c7414c893
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-11 14:26:07 +0900

    feat: 미리보기 시트 디자인 및 데이터 바인딩

--- frontend/src/features/search/components/ResultSheet.tsx
+++ frontend/src/features/search/components/ResultSheet.tsx
@@ -1,15 +1,28 @@
 import { createPortal } from 'react-dom';
 import styled from 'styled-components';
+import Thumbnail from '@/features/songs/components/Thumbnail';
+import Flex from '@/shared/components/Flex/Flex';
+import type { ArtistSearchPreview } from '../types/search';
 
-const ResultSheet = () => {
+interface ResultSheetProps {
+  result: ArtistSearchPreview[];
+}
+
+const ResultSheet = ({ result }: ResultSheetProps) => {
+  const hasResult = result.length > 0;
+
   return createPortal(
     <SheetContainer>
-      <SheetTitle>아티스트</SheetTitle>
-      <TestList>
-        <TestItem>1</TestItem>
-        <TestItem>2</TestItem>
-        <TestItem>3</TestItem>
-      </TestList>
+      <SheetTitle $align="center">아티스트</SheetTitle>
+      <PreviewItemList as="ul" $direction="column" $gap={16} aria-label="검색 결과 미리보기">
+        {result.map(({ id, singer, singerImageUrl }, idx) => (
+          <PreviewItem key={id} as="li" $align="center" $gap={16} aria-label={`${idx}번 ${singer}`}>
+            <Thumbnail src={singerImageUrl} alt="" size="sm" />
+            <Singer aria-disabled>{singer}</Singer>
+          </PreviewItem>
+        ))}
+      </PreviewItemList>
+      {!hasResult && <DefaultMessage>검색 결과가 없습니다</DefaultMessage>}
     </SheetContainer>,
     document.body
   );
@@ -23,13 +36,18 @@ const SheetContainer = styled.div`
   top: 70px;
   right: 12.33%;
 
-  width: 400px;
-  min-height: auto;
+  overflow-y: scroll;
+
+  width: 340px;
+  height: auto;
+  max-height: 320px;
+  padding: 0 16px 16px 16px;
 
   color: white;
 
-  background-color: #161f18;
+  background-color: #121212;
   border-radius: 8px;
+  box-shadow: 0px 0px 10px #ffffff49;
 
   @media (max-width: ${({ theme }) => theme.breakPoints.xxl}) {
     right: 8.33%;
@@ -50,6 +68,38 @@ const SheetContainer = styled.div`
   }
 `;
 
-const SheetTitle = styled.p``;
-const TestList = styled.ul``;
-const TestItem = styled.li``;
+const SheetTitle = styled(Flex)`
+  position: sticky;
+  top: 0;
+  left: 0;
+
+  height: 50px;
+
+  font-weight: 700;
+  letter-spacing: 2px;
+
+  background-color: #121212;
+`;
+
+const PreviewItemList = styled(Flex)``;
+
+const PreviewItem = styled(Flex)`
+  cursor: pointer;
+
+  width: 100%;
+  height: 64px;
+  padding: 8px;
+
+  background-color: ${({ theme: { color } }) => color.black400};
+  border-radius: 4px;
+
+  &:hover {
+    background-color: ${({ theme: { color } }) => color.secondary};
+  }
+`;
+
+const Singer = styled.p`
+  font-weight: 700;
+  letter-spacing: 1px;
+`;
+const DefaultMessage = styled.p``;
