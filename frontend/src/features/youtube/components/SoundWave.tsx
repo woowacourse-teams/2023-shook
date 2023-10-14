251865d2edd1af7006c87613f8e1c42e925c73ab
ukkodeveloper <ukkodeveloper@gmail.com>
2023-10-14 20:37:38 +0900

    refactor: WaveScrubber 구현에 있어 필요없는 컴포넌트 삭제 및 한 데 두기

--- frontend/src/features/youtube/components/SoundWave.tsx
+++ frontend/src/features/youtube/components/SoundWave.tsx
@@ -1,23 +1,24 @@
 import styled from 'styled-components';
 
 interface SoundWaveProps {
-  waveLength: number;
+  length: number;
 }
-
-const SoundWave = ({ waveLength }: SoundWaveProps) => {
-  return Array.from({ length: waveLength }, () => (
-    <>
-      <LongBar />
-      <ShortBar />
-    </>
-  ));
+const SoundWave = ({ length }: SoundWaveProps) => {
+  return Array.from({ length }, (_, index) => {
+    return (
+      <>
+        <LongBar key={`long${index}`} />
+        <ShortBar key={`short${index}`} />
+      </>
+    );
+  });
 };
 
 export default SoundWave;
 
 const LongBar = styled.div`
   z-index: 2;
-  left: calc(50%);
+  left: 50%;
 
   width: 4px;
   height: 24px;
--- frontend/src/features/youtube/components/WaveScrubber.tsx
+++ frontend/src/features/youtube/components/WaveScrubber.tsx
@@ -1,61 +1,33 @@
-import { useRef } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import useVoteInterfaceContext from '@/features/songs/hooks/useVoteInterfaceContext';
-import PlayerBadge from '@/features/youtube/components/PlayerBadge';
-import PlayingToggleButton from '@/features/youtube/components/PlayingToggleButton';
-import ScrubberProgress, {
-  ScrubberProgressAllPlaying,
-} from '@/features/youtube/components/ScrubberProgress';
 import SoundWave from '@/features/youtube/components/SoundWave';
 import useVideoPlayerContext from '@/features/youtube/hooks/useVideoPlayerContext';
 import Flex from '@/shared/components/Flex/Flex';
-import Spacing from '@/shared/components/Spacing';
 import useDebounceEffect from '@/shared/hooks/useDebounceEffect';
-import { secondsToMinSec, toMinSecText } from '@/shared/utils/convertTime';
+import { secondsToMinSec } from '@/shared/utils/convertTime';
 
-const PROGRESS_WIDTH = 350;
 const WaveScrubber = () => {
   const {
-    interval,
     partStartTime,
+    interval,
     videoLength,
     isVideoStatePlaying,
     isAllPlay,
     updatePartStartTime,
-    toggleAllPlay,
   } = useVoteInterfaceContext();
   const { videoPlayer, seekTo } = useVideoPlayerContext();
-  const temptKey = useRef(0);
   const maxPartStartTime = videoLength - interval;
-  const seekAndPlay = () => {
-    seekTo(partStartTime);
-  };
-
-  const partStartTimeText = toMinSecText(partStartTime);
 
   const changePartStartTime: React.UIEventHandler<HTMLDivElement> = (e) => {
     const { scrollWidth, scrollLeft } = e.currentTarget;
-    const unit = (scrollWidth - PROGRESS_WIDTH) / maxPartStartTime;
+    const unit = (scrollWidth - 350) / maxPartStartTime;
     const partStartTimeToChange = Math.floor(scrollLeft / unit);
 
     if (partStartTimeToChange >= 0 && partStartTimeToChange <= maxPartStartTime) {
       const { minute, second } = secondsToMinSec(partStartTimeToChange);
       updatePartStartTime('minute', minute);
       updatePartStartTime('second', second);
-      temptKey.current += temptKey.current + 1;
     }
-  };
-
-  const clickPlay = () => {
-    if (isAllPlay) {
-      videoPlayer.current?.playVideo();
-    } else {
-      seekTo(partStartTime);
-    }
-  };
-
-  const clickPause = () => {
-    videoPlayer.current?.pauseVideo();
   };
 
   const playWhenTouch = () => {
@@ -64,63 +36,35 @@ const WaveScrubber = () => {
     }
   };
 
-  useDebounceEffect<[number, number]>(seekAndPlay, [partStartTime, interval], 300);
+  useDebounceEffect<[number, number]>(() => seekTo(partStartTime), [partStartTime, interval], 300);
 
   return (
     <>
-      <BadgeContainer>
-        <PlayerBadge>{partStartTimeText}</PlayerBadge>
-        <PlayerBadge>
-          <PlayingToggleButton
-            pause={clickPause}
-            play={clickPlay}
-            isPlaying={isVideoStatePlaying}
-          />
-        </PlayerBadge>
-        <PlayerBadge isActive={isAllPlay}>
-          <button onClick={toggleAllPlay}>전체 듣기</button>
-        </PlayerBadge>
-      </BadgeContainer>
-      <Spacing direction="vertical" size={8} />
       <Container>
-        <Flex
+        <WaveWrapper
           onScroll={changePartStartTime}
           onTouchStart={playWhenTouch}
           $gap={8}
-          $css={{
-            zIndex: 3,
-            backgroundColor: 'transparent',
-            alignItems: 'center',
-            overflowX: 'scroll',
-            width: '100%',
-            height: '80px',
-            padding: '0 calc((100% - 150px) / 2)',
-          }}
+          $align="center"
         >
-          <SoundWave waveLength={maxPartStartTime} />
-        </Flex>
-        <PlayingBox />
-        {isVideoStatePlaying && !isAllPlay && <ScrubberProgress interval={interval} />}
-        {isVideoStatePlaying && isAllPlay && <ScrubberProgressAllPlaying />}
+          <SoundWave length={maxPartStartTime} />
+        </WaveWrapper>
+        <ProgressFrame />
+        {isVideoStatePlaying && !isAllPlay && <ProgressFill $interval={interval} />}
+        {isVideoStatePlaying && isAllPlay && <WaveFill />}
       </Container>
     </>
   );
 };
 
 export default WaveScrubber;
-
-const PlayingBox = styled.div`
-  position: absolute;
-  z-index: 1;
-  left: 50%;
-  transform: translateX(-50%);
-
-  width: 150px;
-  height: 50px;
-
-  border: transparent;
-  border-radius: 4px;
-  box-shadow: 0 0 0 2px inset ${({ theme: { color } }) => color.white};
+const WaveWrapper = styled(Flex)`
+  z-index: 3;
+  background-color: transparent;
+  overflow-x: scroll;
+  width: 100%;
+  height: 75px;
+  padding: 0 calc((100% - 150px) / 2);
 `;
 
 const Container = styled.div`
@@ -143,8 +87,92 @@ const Container = styled.div`
   }
 `;
 
-const BadgeContainer = styled.div`
-  display: flex;
-  column-gap: 14px;
-  justify-content: flex-end;
+const ProgressFrame = styled.div`
+  position: absolute;
+  z-index: 1;
+  left: 50%;
+  transform: translateX(-50%);
+
+  width: 150px;
+  height: 50px;
+
+  border: transparent;
+  border-radius: 4px;
+  box-shadow: 0 0 0 2px inset ${({ theme: { color } }) => color.white};
+`;
+
+const fillAnimation = keyframes`
+  0% {
+    background-position: right;
+  }
+  100% {
+    background-position: left;
+  }
+`;
+
+const ProgressFill = styled.div<{ $interval: number }>`
+  pointer-events: none;
+
+  position: absolute;
+  z-index: 0;
+  left: 50%;
+  transform: translateX(-50%);
+
+  width: 150px;
+  height: 50px;
+
+  background: linear-gradient(to left, transparent 50%, pink 50%);
+  background-size: ${({ $interval }) => 200 + (30 - $interval)}%;
+  border-radius: 5px;
+
+  transition: 10s linear;
+  animation: ${fillAnimation} ${({ $interval }) => $interval}s linear infinite;
+`;
+
+const waveFillAnimate = keyframes`
+  0%, 100% {
+    clip-path: polygon(
+      0% 45%,
+      16% 44%,
+      33% 50%,
+      54% 60%,
+      70% 61%,
+      84% 59%,
+      100% 52%,
+      100% 100%,
+      0% 100%
+    );
+  }
+
+  50% {
+    clip-path: polygon(
+      0% 60%,
+      15% 65%,
+      34% 66%,
+      51% 62%,
+      67% 50%,
+      84% 45%,
+      100% 46%,
+      100% 100%,
+      0% 100%
+    );
+  }
+`;
+
+const WaveFill = styled.div`
+  pointer-events: none;
+
+  position: absolute;
+  z-index: 0;
+  left: 50%;
+  transform: translateX(-50%);
+
+  width: 150px;
+  height: 50px;
+
+  background: linear-gradient(to left, deeppink, pink);
+  border-radius: 5px;
+
+  transition: 10s linear;
+  animation: ${waveFillAnimate} 4s ease-in-out infinite;
 `;
