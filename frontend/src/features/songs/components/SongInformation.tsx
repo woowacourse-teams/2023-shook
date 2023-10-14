dfa6528163323854a0c88757eaf9fb5e182080e3
ukkodeveloper <ukkodeveloper@gmail.com>
2023-10-14 20:43:47 +0900

    refactor: 앨범 자켓, 가수, 제목 컨텐츠를 컴포넌트로 분리

---
+++ frontend/src/features/songs/components/SongInformation.tsx
@@ -0,0 +1,56 @@
+import { styled } from 'styled-components';
+import Thumbnail from '@/features/songs/components/Thumbnail';
+import Flex from '@/shared/components/Flex/Flex';
+
+interface SongInformationProps {
+  albumCoverUrl: string;
+  title: string;
+  singer: string;
+}
+
+const SongInformation = ({ albumCoverUrl, title, singer }: SongInformationProps) => {
+  return (
+    <Flex $gap={12} $align="center">
+      <Thumbnail size="sm" src={albumCoverUrl} alt={`${title} 앨범 자켓`} />
+      <SongTextContainer>
+        <SongTitle>{title}</SongTitle>
+        <Singer>{singer}</Singer>
+      </SongTextContainer>
+    </Flex>
+  );
+};
+
+export default SongInformation;
+
+const SongTextContainer = styled.div`
+  overflow: hidden;
+  max-width: 100%;
+`;
+
+const SongTitle = styled.p`
+  overflow: hidden;
+
+  font-size: 22px;
+  font-weight: 700;
+  color: ${({ theme: { color } }) => color.white};
+  text-overflow: ellipsis;
+  white-space: nowrap;
+
+  @media (max-width: ${({ theme }) => theme.breakPoints.md}) {
+    font-size: 18px;
+  }
+`;
+
+const Singer = styled.p`
+  overflow: hidden;
+
+  font-size: 16px;
+  font-weight: 700;
+  color: ${({ theme: { color } }) => color.subText};
+  text-overflow: ellipsis;
+  white-space: nowrap;
+
+  @media (max-width: ${({ theme }) => theme.breakPoints.md}) {
+    font-size: 14px;
+  }
+`;
