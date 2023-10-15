80079d460d558a243e584e115f354eee6cb696a7
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-15 21:20:43 +0900

    feat: 백드롭 추가

--- frontend/src/features/search/components/SearchPreviewSheet.tsx
+++ frontend/src/features/search/components/SearchPreviewSheet.tsx
@@ -21,37 +21,51 @@ const SearchPreviewSheet = ({ result, endSearch }: ResultSheetProps) => {
   };
 
   return (
-    <SheetContainer
-      id="search-preview-sheet"
-      tabIndex={-1}
-      aria-label="아티스트 검색 결과 미리보기"
-    >
-      <FlexSheetTitle $align="center" aria-hidden>
-        아티스트
-      </FlexSheetTitle>
-      <FlexPreviewItemList as="ul" $direction="column" $gap={16}>
-        {result.map(({ id, singer, profileImageUrl }) => (
-          <FlexPreviewItem key={id} as="li">
-            <FlexGoToDetail
-              as="button"
-              type="button"
-              onMouseDown={() => goToSingerDetailPage(id)}
-              aria-label={`${singer} 상세 페이지 바로가기`}
-              $gap={16}
-              $align="center"
-            >
-              <Thumbnail src={profileImageUrl} alt="" size="sm" />
-              <Singer aria-hidden>{singer}</Singer>
-            </FlexGoToDetail>
-          </FlexPreviewItem>
-        ))}
-      </FlexPreviewItemList>
-      {!hasResult && <DefaultMessage>검색 결과가 없습니다</DefaultMessage>}
-    </SheetContainer>
+    <>
+      <Backdrop />
+      <SheetContainer
+        id="search-preview-sheet"
+        tabIndex={-1}
+        aria-label="아티스트 검색 결과 미리보기"
+      >
+        <FlexSheetTitle $align="center" aria-hidden>
+          아티스트
+        </FlexSheetTitle>
+        <FlexPreviewItemList as="ul" $direction="column" $gap={16}>
+          {result.map(({ id, singer, profileImageUrl }) => (
+            <FlexPreviewItem key={id} as="li">
+              <FlexGoToDetail
+                as="button"
+                type="button"
+                onMouseDown={() => goToSingerDetailPage(id)}
+                aria-label={`${singer} 상세 페이지 바로가기`}
+                $gap={16}
+                $align="center"
+              >
+                <Thumbnail src={profileImageUrl} alt="" size="sm" />
+                <Singer aria-hidden>{singer}</Singer>
+              </FlexGoToDetail>
+            </FlexPreviewItem>
+          ))}
+        </FlexPreviewItemList>
+        {!hasResult && <DefaultMessage>검색 결과가 없습니다</DefaultMessage>}
+      </SheetContainer>
+    </>
   );
 };
 
 export default SearchPreviewSheet;
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 80px;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(0, 0, 0, 0.5);
+`;
 
 const SheetContainer = styled.section`
   position: fixed;
@@ -125,7 +139,9 @@ const Singer = styled.p`
   font-weight: 700;
   letter-spacing: 1px;
 `;
-const DefaultMessage = styled.p``;
+const DefaultMessage = styled.p`
+  font-size: 14px;
+`;
 
 const FlexGoToDetail = styled(Flex)`
   width: 100%;
