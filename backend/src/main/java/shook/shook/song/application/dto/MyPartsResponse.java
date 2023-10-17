c5d14f15838868359c827d426b48d5746e0b596e
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-10-17 23:37:49 +0900

    docs: swagger 명세 추가

--- backend/src/main/java/shook/shook/song/application/dto/MyPartsResponse.java
+++ backend/src/main/java/shook/shook/song/application/dto/MyPartsResponse.java
@@ -7,28 +7,32 @@ import lombok.Getter;
 import shook.shook.my_part.domain.MemberPart;
 import shook.shook.song.domain.Song;
 
-@Schema(description = "마이파트 응답")
+@Schema(description = "마이 파트 응답")
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class MyPartsResponse {
 
+    @Schema(description = "노래 id", example = "1")
     private Long songId;
 
+    @Schema(description = "노래 앨범 커버 이미지 url", example = "https://image.com/album-cover.jpg")
     private String albumCoverUrl;
 
+    @Schema(description = "노래 제목", example = "제목")
     private String title;
 
+    @Schema(description = "마이 파트 시작 시간", example = "0")
     private int start;
 
+    @Schema(description = "마이 파트 길이", example = "10")
     private int length;
-
 
     public static MyPartsResponse of(final Song song, final MemberPart memberPart) {
         return new MyPartsResponse(
-                song.getId(),
-                song.getAlbumCoverUrl(),
-                song.getTitle(),
-                memberPart.getStartSecond(),
-                memberPart.getLength());
+            song.getId(),
+            song.getAlbumCoverUrl(),
+            song.getTitle(),
+            memberPart.getStartSecond(),
+            memberPart.getLength());
     }
 }
