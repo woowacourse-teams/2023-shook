8c6d7d1a443855b7cb64699cf52836dfe2be9632
윤정민 <dev.timetravel@gmail.com>
2023-10-17 19:16:44 +0900

    Refactor/#215 댓글 시간을 시간,일,달 등 자세하게 표시한다 (#494)
    * feat: 상대시간 문자열을 반환하는 convertRelativeTimeString 함수 구현
    
    * test: convertRelativeTimeString 함수 테스트 작성
    
    * refactor: Comment 컴포넌트의 상대시간 수정
    
    * test: 설명 추가 및 테스트 케이스 수정
    
    * test: 테스트 케이스 제목 및 주석 수정
    
    * refactor: 상수 단위를 ms로 통일
    
    * refactor: index 기준을 바꾸어 unitIndex가 상대 시간 기준이되는 index임을 명확하게 표현
    
    * style: 상대 시간 기준에 대한 주석 추가

---
+++ frontend/src/shared/utils/convertRelativeTimeString.test.ts
@@ -0,0 +1,61 @@
+import convertRelativeTimeString from './convertRelativeTimeString';
+
+describe('ISO-8601 형식의 string을 받아 현재 시간과 비교해서 상대시간을 반환한다.', () => {
+  // NOTE: 상대 시간 기준은 n <= 상대 시간 < n + 1 입니다.
+  test('차이가 없을 경우 방금 전을 반환한다.', () => {
+    const now = new Date().toISOString();
+
+    expect(convertRelativeTimeString(now)).toBe('방금 전');
+  });
+
+  test('차이가 0초 이상, 1분 미만의 경우 방금 전을 반환한다.', () => {
+    const createdAt = '2023-10-06T09:59:59.999';
+    const now = '2023-10-06T10:00:00';
+
+    expect(convertRelativeTimeString(createdAt, now)).toBe('방금 전');
+  });
+
+  test('차이가 1분 이상, 1시간 미만인 경우 n분 전을 반환한다', () => {
+    const createdAt = '2023-10-06T09:50:10';
+    const now = '2023-10-06T10:00:00';
+
+    expect(convertRelativeTimeString(createdAt, now)).toBe('9분 전');
+  });
+
+  test('차이가 1시간 이상, 1일 미만인 경우 n시간 전을 반환한다.', () => {
+    const createdAt = '2023-10-06T08:12:00';
+    const now = '2023-10-06T10:00:00';
+
+    expect(convertRelativeTimeString(createdAt, now)).toBe('1시간 전');
+  });
+
+  test('차이가 1일 이상, 1주 미만인 경우 n일 전을 반환한다.', () => {
+    const createdAt = '2023-10-03T10:00:00';
+    const now = '2023-10-06T10:00:00';
+
+    expect(convertRelativeTimeString(createdAt, now)).toBe('3일 전');
+  });
+
+  test('차이가 1주 이상, 1달 미만인 경우 n주 전을 반환한다.', () => {
+    const createdAt = '2023-09-11T10:00:00';
+    const now = '2023-10-06T10:00:00';
+
+    expect(convertRelativeTimeString(createdAt, now)).toBe('3주 전');
+  });
+
+  test('차이가 1달 이상, 1년 미만인 경우 n개월 전을 반환한다.', () => {
+    // NOTE: 1달을 30일로 계산하기 때문에 오차가 약간 있습니다.
+    const createdAt = '2023-06-08T10:00:00';
+    const now = '2023-10-06T10:00:00';
+
+    expect(convertRelativeTimeString(createdAt, now)).toBe('4개월 전');
+  });
+
+  test('차이가 1년 이상인 경우 n년 전을 반환한다.', () => {
+    // NOTE: 예시가 3년이 되기 직전 값으로 최대값으로 볼 수 있습니다.
+    const createdAt = '2020-10-06T10:00:00.001';
+    const now = '2023-10-06T10:00:00';
+
+    expect(convertRelativeTimeString(createdAt, now)).toBe('2년 전');
+  });
+});
