19c9bea72d9de0d7247a95ca23740f10e4d52129
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-17 14:36:44 +0900

    refactor: msw 핸들러 고도화 및 픽스쳐 데이터 추가
    검색 완료 페이지에서 쓰이는 api 요청을 처리할 수 있도록 개선

--- frontend/src/mocks/handlers/searchHandlers.ts
+++ frontend/src/mocks/handlers/searchHandlers.ts
@@ -1,24 +1,33 @@
 import { rest } from 'msw';
-import searchedArtistPreview from '@/mocks/fixtures/searchedArtistPreview.json';
+import searchedSinger from '@/mocks/fixtures/searchedSinger.json';
+import searchedSingerPreview from '@/mocks/fixtures/searchedSingerPreview.json';
 
 const { BASE_URL } = process.env;
 
 const searchHandlers = [
   rest.get(`${BASE_URL}/singers`, (req, res, ctx) => {
     const query = req.url.searchParams.get('name') ?? '';
-    const search = req.url.searchParams.get('search');
+    const [singer, song] = req.url.searchParams.getAll('search');
     const testQueries = ['악동뮤지션', '악동', '뮤지션'];
 
-    const isPreviewRequest = search === 'singer';
+    const isPreviewRequest = singer !== undefined && song === undefined;
+    const isSearchRequest = song !== undefined;
+
     const isInTestQueries = testQueries.some(
       (testQuery) => encodeURIComponent(testQuery) === encodeURIComponent(query)
     );
 
     if (isPreviewRequest && isInTestQueries) {
-      return res(ctx.status(200), ctx.json(searchedArtistPreview));
+      return res(ctx.status(200), ctx.json(searchedSingerPreview));
     }
 
-    return res(ctx.status(200), ctx.json([]));
+    if (isSearchRequest && isInTestQueries) {
+      return res(ctx.status(200), ctx.json(searchedSinger));
+    }
+
+    if (!isInTestQueries) {
+      return res(ctx.status(200), ctx.json([]));
+    }
   }),
 ];
 
