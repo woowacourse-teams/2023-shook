726937bcc797ced66bd266431595a900b7dd930f
cruelladevil <dev.timetravel@gmail.com>
2023-10-17 20:47:32 +0900

    fix: fetch의 method를 대문자로 수정

--- frontend/src/features/member/remotes/myPage.ts
+++ frontend/src/features/member/remotes/myPage.ts
@@ -1,5 +1,5 @@
 import fetcher from '@/shared/remotes';
 
-export const getLikeParts = () => fetcher('/my-page/like-parts', 'get');
+export const getLikeParts = () => fetcher('/my-page/like-parts', 'GET');
 
-export const getMyParts = () => fetcher('/my-page/member-parts', 'get');
+export const getMyParts = () => fetcher('/my-page/member-parts', 'GET');
