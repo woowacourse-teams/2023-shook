9db9be2a256259bff6c0be8c4561496db0878ff9
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-10-18 21:45:12 +0900

    chore: 사용하지 않는 import 삭제 및 픽스쳐 데이터 삭제

--- frontend/src/pages/SearchResultPage.tsx
+++ frontend/src/pages/SearchResultPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { getSingerSearch } from '@/features/search/remotes/search';
 import SingerBanner from '@/features/singer/components/SingerBanner';
