8d8e927e8a20e51e96ca09b5fbff17d7df1706f4
Eunsol Kim <61370551+Cyma-s@users.noreply.github.com>
2023-10-19 13:55:55 +0900

    hotfix: dev schema 도메인에 맞게 수정 및 prod schema 롤백

--- backend/src/main/resources/dev/schema.sql
+++ backend/src/main/resources/dev/schema.sql
@@ -2,6 +2,7 @@ drop table if exists song;
 drop table if exists killing_part;
 drop table if exists killing_part_like;
 drop table if exists killing_part_comment;
+drop table if exists voting_song_part;
 drop table if exists voting_song;
 drop table if exists vote;
 drop table if exists member;
@@ -13,8 +14,12 @@ create table if not exists song
     title           varchar(100) not null,
     singer          varchar(50)  not null,
     length          integer      not null,
-    video_url       text         not null,
+    video_id        varchar(20)  not null,
     album_cover_url text         not null,
+    genre           varchar(255) check (genre in
+                                        ('BALLAD', 'DANCE', 'HIPHOP', 'RHYTHM_AND_BLUES', 'INDIE',
+                                         'ROCK_METAL', 'TROT', 'FOLK_BLUES', 'POP', 'JAZZ',
+                                         'CLASSIC', 'J_POP', 'EDM', 'ETC')),
     created_at      timestamp(6) not null,
     primary key (id)
 );
@@ -25,6 +30,7 @@ create table if not exists killing_part
     start_second integer      not null,
     length       integer      not null,
     song_id      bigint       not null,
+    like_count   integer      not null default 0,
     created_at   timestamp(6) not null,
     primary key (id)
 );
@@ -44,6 +50,7 @@ create table if not exists killing_part_comment
 (
     id              bigint auto_increment,
     killing_part_id bigint       not null,
+    member_id       bigint       not null,
     content         varchar(200) not null,
     created_at      timestamp(6) not null,
     primary key (id)
@@ -55,7 +62,7 @@ create table if not exists voting_song
     title           varchar(100) not null,
     singer          varchar(50)  not null,
     length          integer      not null,
-    video_url       text         not null,
+    video_id        varchar(20)  not null,
     album_cover_url text         not null,
     created_at      timestamp(6) not null,
     primary key (id)
@@ -73,15 +80,17 @@ create table if not exists vote
 (
     id                  bigint auto_increment,
     voting_song_part_id bigint       not null,
+    member_id           bigint       not null,
     created_at          timestamp(6) not null,
     primary key (id)
 );
 
 create table if not exists member
 (
-    id       bigint auto_increment,
-    email    varchar(100) not null,
-    nickname varchar(100) not null,
+    id         bigint auto_increment,
+    email      varchar(100) not null,
+    nickname   varchar(100) not null,
+    created_at timestamp(6) not null,
     primary key (id)
 );
 
