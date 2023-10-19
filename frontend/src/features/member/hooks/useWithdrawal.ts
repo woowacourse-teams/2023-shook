ba945b913d692af037232343280c8e70309672a5
ukkodeveloper <ukkodeveloper@gmail.com>
2023-10-19 09:50:45 +0900

    feat: nickname 삭제 api

--- frontend/src/features/member/hooks/useWithdrawal.ts
+++ frontend/src/features/member/hooks/useWithdrawal.ts
@@ -7,7 +7,12 @@ import { useMutation } from '@/shared/hooks/useMutation';
 const useWithdrawal = () => {
   const navigate = useNavigate();
   const { user, logout } = useAuthContext();
-  const { mutateData: withdrawMember } = useMutation(deleteMember(user?.memberId));
+  // TODO: 피드백 반영하여 error throw. 그러나 error 핸들링 반드시 필요
+  if (!user) {
+    throw new Error('현재 user 로그인된 정보가 없습니다.');
+  }
+
+  const { mutateData: withdrawMember } = useMutation(() => deleteMember(user.memberId));
 
   const handleWithdrawal = async () => {
     await withdrawMember();
