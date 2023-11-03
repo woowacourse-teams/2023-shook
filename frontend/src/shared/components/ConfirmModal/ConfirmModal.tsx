3a8246eb3fd7981a1014cdd37bc3b64b3f27ea0b
ukkodeveloper <ukkodeveloper@gmail.com>
2023-11-03 18:31:34 +0900

    feat: confirm modal 수락 기능 추가

--- frontend/src/shared/components/ConfirmModal/ConfirmModal.tsx
+++ frontend/src/shared/components/ConfirmModal/ConfirmModal.tsx
@@ -4,7 +4,9 @@ import { Flex } from 'shook-layout';
 import styled from 'styled-components';
 import type { ReactNode } from 'react';
 
-const ConfirmContext = createContext<null>(null);
+const ConfirmContext = createContext<null | {
+  getConfirmation: (modalState: ModalState) => Promise<boolean>;
+}>(null);
 
 const Backdrop = styled.div``;
 const ContainerFlex = styled(Flex)``;
@@ -17,12 +19,24 @@ const ConfirmButton = styled.button``;
 interface ModalState {
   title: string;
   content: ReactNode;
-  cancelName: string;
-  confirmName: string;
+  cancelName?: string;
+  confirmName?: string;
 }
+
+type Resolver<T> = (value: T) => void;
+
+const createPromise = <T,>() => {
+  let resolver: Resolver<T> | null = null;
+  const promise = new Promise<T>((resolve) => {
+    resolver = resolve;
+  });
+
+  return { promise, resolver };
+};
 
 const ConfirmProvider = (children: ReactNode) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [resolve, setResolve] = useState<((value: boolean) => void) | null>(null);
   const [modalState, setModalState] = useState<ModalState>({
     title: '',
     content: '',
@@ -31,8 +45,33 @@ const ConfirmProvider = (children: ReactNode) => {
   });
   const { title, content, cancelName, confirmName } = modalState;
 
+  const getConfirmation = ({
+    cancelName = '닫기',
+    confirmName = '확인',
+    ...restState
+  }: ModalState): Promise<boolean> => {
+    const { promise, resolver } = createPromise<boolean>();
+    setResolve(resolver);
+    setModalState({
+      cancelName,
+      confirmName,
+      ...restState,
+    });
+    setIsOpen(true);
+
+    return promise;
+  };
+
+  const onClick = (status: boolean) => {
+    setIsOpen(false);
+
+    if (resolve) {
+      resolve(status);
+    }
+  };
+
   return (
-    <ConfirmContext.Provider value={null}>
+    <ConfirmContext.Provider value={{ getConfirmation }}>
       {children}
       {isOpen &&
         createPortal(
@@ -42,8 +81,8 @@ const ConfirmProvider = (children: ReactNode) => {
               <Title>{title}</Title>
               <Content>{content}</Content>
               <ButtonFlex>
-                <CancelButton>{cancelName}</CancelButton>
-                <ConfirmButton>{confirmName}</ConfirmButton>
+                <CancelButton onClick={() => onClick(false)}>{cancelName}</CancelButton>
+                <ConfirmButton onClick={() => onClick(true)}>{confirmName}</ConfirmButton>
               </ButtonFlex>
             </ContainerFlex>
           </>,
