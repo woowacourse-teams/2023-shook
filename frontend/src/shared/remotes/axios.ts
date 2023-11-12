124af5321d538b3d3c3e6f821115a800e1d7367e
DOBOB_LAPTOP <creativedobob@gmail.com>
2023-11-12 21:17:13 +0900

    feat: axios 인스턴스 및 인터셉터 구현

---
+++ frontend/src/shared/remotes/axios.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import { postRefreshAccessToken } from './auth';
+import type { AxiosError, AxiosRequestConfig, InternalAxiosRequestConfig } from 'axios';
+
+const { BASE_URL } = process.env;
+
+const defaultConfig: AxiosRequestConfig = {
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+};
+
+export const clientBasic = axios.create(defaultConfig);
+export const client = axios.create(defaultConfig);
+
+// 요청 인터셉터
+const setToken = (config: InternalAxiosRequestConfig) => {
+  const accessToken = localStorage.getItem('userToken');
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+};
+
+// 응답 인터셉터
+const refreshAccessTokenOnAuthError = async (error: AxiosError) => {
+  const config = error.config;
+
+  if (error.response?.status === 401 && config?.headers.Authorization) {
+    try {
+      const staleAccessToken = localStorage.getItem('userToken') ?? '';
+      const { accessToken } = await postRefreshAccessToken(staleAccessToken);
+
+      localStorage.setItem('userToken', accessToken);
+      config.headers.Authorization = `Bearer ${accessToken}`;
+
+      return client(config);
+    } catch {
+      // window.alert('세션이 만료되었습니다. 다시 로그인 해주세요');
+    }
+  }
+
+  return Promise.reject(error);
+};
+
+client.interceptors.request.use(setToken);
+client.interceptors.response.use((response) => response, refreshAccessTokenOnAuthError);
+
+export default client;
