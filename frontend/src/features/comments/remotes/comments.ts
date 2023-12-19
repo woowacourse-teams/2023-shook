6a125194d6389b2d09df4cb3287f707d3da4ce7e
creative-Lee <creativedobob@gmail.com>
2023-12-19 16:39:17 +0900

    refactor: 코멘트 작성 로직 React Query 적용
    1. useQuery, useMutation hook으로 기존 로직 대체
    2. remote 함수 인자타입 변경 - mutateFn의 함수는 인자를 1개로 제한
    3. onSuccess는 useMutation 과 mutate 함수 순서로 실행됨

--- frontend/src/features/comments/remotes/comments.ts
+++ frontend/src/features/comments/remotes/comments.ts
@@ -1,7 +1,15 @@
 import { client } from '@/shared/remotes/axios';
 import type { Comment } from '../types/comment.type';
 
-export const postComment = async (songId: number, partId: number, content: string) => {
+export const postComment = async ({
+  songId,
+  partId,
+  content,
+}: {
+  songId: number;
+  partId: number;
+  content: string;
+}) => {
   await client.post(`/songs/${songId}/parts/${partId}/comments`, { content });
 };
 
