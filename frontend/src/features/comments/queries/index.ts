2130edf44e2fd39f4a6f8d07521503a82fe33d0d
윤정민 <dev.timetravel@gmail.com>
2024-10-12 16:25:44 +0900

    refactor: mutate 옵션이 드러나도록 컴포넌트에서 직접 사용하도록 변경

--- frontend/src/features/comments/queries/index.ts
+++ frontend/src/features/comments/queries/index.ts
@@ -1,21 +1,8 @@
-import { queryOptions, useMutation, useQueryClient } from '@tanstack/react-query';
-import { getComments, postComment } from '../remotes/comments';
+import { queryOptions } from '@tanstack/react-query';
+import { getComments } from '../remotes/comments';
 
 export const commentsQueryOptions = (songId: number, partId: number) =>
   queryOptions({
     queryKey: ['comments', songId, partId],
     queryFn: () => getComments(songId, partId),
   });
-
-export const usePostCommentMutation = () => {
-  const client = useQueryClient();
-
-  const { mutate: postNewComment, ...mutations } = useMutation({
-    mutationFn: postComment,
-    onSuccess: (_, { songId, partId }) => {
-      client.invalidateQueries({ queryKey: ['comments', songId, partId] });
-    },
-  });
-
-  return { postNewComment, mutations };
-};
